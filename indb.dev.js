ee64a590645277edf5318d4ac0e32166b076b3ba
Taylor Buley <taylor.buley@gmail.com>
2011-10-16 18:44:07 -0400

    update

--- indb.dev.js
+++ indb.dev.js
@@ -1093,7 +1093,7 @@ InDB.cursor.direction.previous = function( no_dupes ) {
 
 InDB.cursor.isDirection = function( direction ) {
 	console.log("IS DIRECTION?",direction);
-	direction = ( 'undefined' !== typeof direction && InDB.isNumber( direction ) && direction > InDB.cursor.direction.next() && direction <= InDB.cursor.direction.previous( true ) ) ? true : false;
+	direction = ( 'undefined' !== typeof direction && InDB.isNumber( direction ) && direction >= InDB.cursor.direction.next() && direction <= InDB.cursor.direction.previous( true ) ) ? true : false;
 	console.log("IS DIRECTION",direction);
 	return direction;
 };
