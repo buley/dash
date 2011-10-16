77e397cf94b623980903949eb9e2574e824cbaeb
Taylor Buley <taylor.buley@gmail.com>
2011-10-15 22:28:22 -0400

    update

--- indb.dev.js
+++ indb.dev.js
@@ -2565,15 +2565,18 @@ InDB.cursor.update = function ( store, index, keyRange, data, replace, expecting
 				}
 				if( 'undefined' !== typeof expecting && null !== expecting && 'undefined' !== result[ attr ] && 'undefined' !== typeof expecting[ attr ] && null !== expecting[ attr ] ) {
 
-					if( !!InDB.debug ) {
-						console.log( 'InDB.row.update > value was not expected.', result[ attr ], expecting[ attr ] );
+					if( result[ attr ] == value ) {
+
+						if( !!InDB.debug ) {
+							console.log( 'InDB.row.update > value was not expected.', result[ attr ], expecting[ attr ] );
+						}
+
+						var err = new Error( 'Found ' + result[ attr] + ', expecting ' + expecting[ attr ] );
+						context[ 'event' ] = err;
+
+						on_error( context );
+
 					}
-
-					var err = new Error( 'Found ' + result[ attr] + ', expecting ' + expecting[ attr ] );
-					context[ 'event' ] = err;
-
-					on_error( context );
-
 				       	
 					if( !InDB.assert( result[ attr ] == value, 'Found ' + result[ attr ] + ', expecting ' + expecting[ attr ] ) ) {
 						return;
