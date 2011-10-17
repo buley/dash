ac19686d9dafca4b00474ee74c758d792909e165
Taylor Buley <taylor.buley@gmail.com>
2011-10-16 20:52:23 -0400

    update

--- indb.dev.js
+++ indb.dev.js
@@ -2884,6 +2884,7 @@ InDB.cursor.delete = function ( store, index, keyRange, direction, limit, on_suc
 			console.log('JAMAICA',limit,total,cursor);
 			if( 'undefined' == typeof limit || null == limit || total < limit ) {
 				cursor[ 'delete' ]();
+				console.log('deleted already, now continuing');
 				cursor[ 'continue' ]();
 			}
 		}
