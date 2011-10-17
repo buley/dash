1e5e3cb0330a1f8694c8dc64d2d9320b90910f4f
Taylor Buley <taylor.buley@gmail.com>
2011-10-16 21:53:46 -0400

    update

--- indb.dev.js
+++ indb.dev.js
@@ -1614,7 +1614,6 @@ InDB.row.add = function ( store, data, on_success, on_error, on_abort, on_comple
 	if ( !!InDB.debug ) {
 		console.log ( 'InDB.row.add', store, data, on_success, on_error, on_abort );
 	}
-	console.log('sucbjecty change',on_success);
 
         /* Assertions */
 
@@ -1681,6 +1680,7 @@ InDB.row.add = function ( store, data, on_success, on_error, on_abort, on_comple
 			context[ 'event' ] = event;
 	
 
+			console.log("CAALLLLLL");
 			/* Callback */
 
 			on_success( context );
