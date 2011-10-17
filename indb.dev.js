2cf59e391d7b34194b6e770ecdc3b52f1b5e6a57
Taylor Buley <taylor.buley@gmail.com>
2011-10-17 00:18:44 -0400

    update

--- indb.dev.js
+++ indb.dev.js
@@ -2628,6 +2628,7 @@ InDB.cursor.update = function ( store, index, keyRange, data, direction, limit, 
 				console.log('Neural.synapses.cursor.update', JSON.stringify( instance_data ) );
 			}
 		}
+		console.log('instnce',JSON.stringify( instance_data ) );
 		if( false == replace && null !== result && 'undefined' !== result ) {	
 			var temp_data = instance_data;
 			for( attr in result ) {
