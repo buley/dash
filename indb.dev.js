82fcbb8ac373f8200eaa3ff97532d0246df03896
Taylor Buley <taylor.buley@gmail.com>
2011-10-22 16:15:46 -0400

    not going so hot

--- indb.dev.js
+++ indb.dev.js
@@ -3462,10 +3462,10 @@ var IDB = (function(){
 		var data = request.data;
 		var new_data;
 		if( 'function' !== typeof data ) {
-			new_data = DB.prototype.shorthand_encode( { 'store': request.store, 'data': data } );
+			new_data = InDB.shorthand.encode( { 'store': request.store, 'data': data } );
 		} else {
 			new_data = function( arg ) {
-				return DB.prototype.shorthand_encode( { 'store': request.store, 'data': data( DB.prototype.shorthand_decode( arg ) ) } );
+				return InDB.shorthand.encode( { 'store': request.store, 'data': data( DB.prototype.shorthand_decode( arg ) ) } );
 			};
 		}
 
