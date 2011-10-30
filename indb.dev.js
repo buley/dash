7a97cdbe19d7e64cc295879890641db14239cea0
Taylor Buley <taylor.buley@gmail.com>
2011-10-29 22:49:33 -0400

    via vim

--- indb.dev.js
+++ indb.dev.js
@@ -3733,7 +3733,6 @@ var IDB = (function(){
 		var on_success = function( context ) {
 			if( 'function' == typeof request.on_success ) {
 				var value = InDB.row.value( context );
-				console.log("PUT SUCC",value);
 				request.on_success( value );
 			}
 		}
