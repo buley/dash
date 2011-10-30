6c91be112cc0e562d6de505de36c57bcbdbe4cb9
Taylor Buley <taylor.buley@gmail.com>
2011-10-30 00:12:11 -0400

    via vim

--- indb.dev.js
+++ indb.dev.js
@@ -3229,7 +3229,7 @@ var IDB = (function(){
 			if( 'undefined' !== typeof expecting && null !== expecting ) {	
 				for ( attr in expecting ) {
 					if( 'function' == typeof expecting[ attr ] ) {
-						expecting[ attr ] = expecting[ attr ]( result[ attr ] );
+						expecting[ attr ] = expecting[ attr ]( cursor_result[ attr ] );
 					}
 					console.log( 'exp',attr,expecting,cursor_result);
 					if( 'undefined' !== typeof cursor_result && 'undefined' !== typeof cursor_result[ attr ] && 'undefined' !== typeof expecting[ attr ] && null !== expecting[ attr ] && cursor_result[ attr ] !== expecting[ attr ] ) {
