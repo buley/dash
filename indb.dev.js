04d30cfca5e9cd827a733163bd59ae6949a8e53b
Taylor Buley <taylor.buley@gmail.com>
2011-11-03 01:03:26 -0400

    via vim

--- indb.dev.js
+++ indb.dev.js
@@ -3760,7 +3760,7 @@ var IDB = (function(){
 		var on_success = function( context ) {
 
 			if( 'function' == typeof request.on_success ) {
-		console.log('NEGATIVE', context );
+		console.log('NEGATIVE', request.on_success, value );
 				var value = InDB.row.value( context );
 				request.on_success( value );
 			}
