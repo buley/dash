36cefbe17f17733f0df5d0f3f444916929778601
Taylor Buley <taylor.buley@gmail.com>
2011-11-02 21:39:13 -0400

    via vim

--- indb.dev.js
+++ indb.dev.js
@@ -3811,9 +3811,6 @@ var IDB = (function(){
 		var store = request.store;
 		store = ( !InDB.isEmpty( store ) ) ? store : current_store;
 
-		var properties = request.properties;
-		properties = ( !InDB.isEmpty( properties ) ) ? properties : null;
-
 		var data = request.data;
 		var new_data;
 		if( 'function' !== typeof data ) {
@@ -3829,7 +3826,7 @@ var IDB = (function(){
 			expected = InDB.shorthand.encode( { 'store': store, 'data': expected } );
 		}
 
-		InDB.trigger( 'InDB_do_row_update', { 'store': store, 'key': request.key, 'index': request.index, 'data': new_data, 'properties': properties, 'replace': request.replace, 'expected': expected, 'on_success': on_success, 'on_error': on_error, 'on_abort': request.on_abort } );
+		InDB.trigger( 'InDB_do_row_update', { 'store': store, 'key': request.key, 'index': request.index, 'data': new_data, 'replace': request.replace, 'expected': expected, 'on_success': on_success, 'on_error': on_error, 'on_abort': request.on_abort } );
 
 		return this;
 
@@ -3875,6 +3872,11 @@ var IDB = (function(){
 		/* Setup */
 
 		var keyRange = InDB.range.get( key, begin, end, left_inclusive, right_inclusive );
+
+
+		/* Defaults */
+
+		properties = ( !InDB.isEmpty( properties ) ) ? properties : null;
 
 
 		/* Callbacks */
