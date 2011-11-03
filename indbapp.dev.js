0126f85ceaffcb3784d32293b8dc2fe8869eb9b6
Taylor Buley <taylor.buley@gmail.com>
2011-11-03 01:05:39 -0400

    via vim

--- indbapp.dev.js
+++ indbapp.dev.js
@@ -70,7 +70,7 @@ var InDBApp = (function() {
 			if( !!debug ) {
 				console.log( 'App.prototype.add success', value );
 			}
-			console.log("WPOMENS GROUP",value);
+			console.log("WPOMENS GROUP",request.on_success);
 			/* Callback */
 			if( 'function' == typeof request.on_success ) {
 				request.on_success( value );
