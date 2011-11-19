eb0795203b8e2491b8fe7ded49df4ea593bb1ced
Taylor Buley <taylor.buley@gmail.com>
2011-11-19 14:46:07 -0500

    adding a method to build the network

--- indb.dev.js
+++ indb.dev.js
@@ -2151,7 +2151,9 @@ var IDB = (function(){
 							var thing = data[ attr ];
 							if( 'function' === typeof thing ) {
 								var result_value = result;
+								console.log("SUMMONING MOTH",result_value[attr]);
 								data[ attr ] = thing( result_value[ attr ] );
+								console.log("MOTH SUMMONED",data[attr]);
 								if( !!InDB.debug ) {
 									console.log('InDB.cursor.update parsed data fn', JSON.stringify( data ) );
 								}
