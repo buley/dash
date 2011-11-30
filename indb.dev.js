8c3613516f0f31e4523b66c49e9c849cbd9192e4
Taylor Buley <taylor.buley@gmail.com>
2011-11-30 00:58:45 -0500

    via vim

--- indb.dev.js
+++ indb.dev.js
@@ -196,7 +196,6 @@ var IDB = (function(){
 				reversed[ InDB.shorthand.get( { 'store': store, 'key': item } ) ] = item;
 			}
 		}
-		console.log('reversed',store,k,reversed[k],shorthand_map,reversed);
 		if( 'undefined' !== typeof reversed[ k ] ) {
 			return reversed[ k ];
 		} else {
