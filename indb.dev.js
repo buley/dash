41c99983a657a6b24af3dfb58a2b16ad1b70af20
Taylor Buley <taylor.buley@gmail.com>
2011-12-09 00:21:06 -0500

    update

--- indb.dev.js
+++ indb.dev.js
@@ -5,9 +5,8 @@
  **/
 
 /*
- *
  * The MIT License (MIT)
- * Copyright (c) <year> <copyright holders>
+ * Copyright (c) 2011 Buley LLC
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -90,7 +89,7 @@ var IDB = (function(){
 
 	InDB.database.version = 1;
 
-	InDB.debug = true;
+	InDB.debug = false;
 
 	InDB.events.onComplete = function ( e ) {
 		if ( !!InDB.debug ) {
