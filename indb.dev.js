ec0b52338b65ceeb55d79e7aac715046822ca4b4
Taylor Buley <taylor.buley@gmail.com>
2012-03-13 01:11:48 -0400

    ...

--- indb.dev.js
+++ indb.dev.js
@@ -3938,11 +3938,13 @@ var IDB = (function(){
 	/* Database */
 
 	DB.prototype.install = function ( request ) {
+		console.log("STORE INSTALL");
 		console.trace();
 		var store = request.store;
 
 		DB.prototype.store.create( { 'store': store, 'indexes': request.indexes, 'on_success': function( store_event ) {
-			console.log("STORE EVENT",store_event);
+			console.trace();
+			console.log("STORE EVENT");
 			DB.prototype.index.create( { 'store': store, 'indexes': request.indexes, 'on_success': function( result ) {
 				console.log( 'DB.install() index success' );
 				if( 'function' === typeof request.on_success ) {
