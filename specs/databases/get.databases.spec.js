e5a606b582096a414044c8e74ec40e222f3f6672
Taylor Buley <taylor.buley@gmail.com>
2014-02-02 23:51:56 -0500

    .

--- specs/databases/get.databases.spec.js
+++ specs/databases/get.databases.spec.js
@@ -12,7 +12,6 @@
 			ctx;	
 		it( 'should get all databases, when available', function() {
 			dash.get.databases()
-				.then(dash.open.databases)
 				.then(function(context) {
 					ctx = context;
 					isFinished = true;
@@ -34,7 +33,7 @@
 						this.notify = notify;
 					});
 					it("should be a success when available", function() {
-						if (undefined !== window.indexedDB.webkitGetDatabaseNames) {
+						if (undefined !== window.indexedDB && undefined !== window.indexedDB.webkitGetDatabaseNames) {
 							expect(this.success).toBe(true);
 							expect(this.error).toBe(false);
 							expect(this.notify).toBe(false);
@@ -42,7 +41,7 @@
 						}
 					});
 					it("should be an error when unavailable", function() {
-						if (undefined === window.indexedDB.webkitGetDatabaseNames) {
+						if (undefined === window.indexedDB || undefined === window.indexedDB.webkitGetDatabaseNames) {
 							expect(this.success).toBe(false);
 							expect(this.error).toBe(true);
 							expect(this.notify).toBe(false);
