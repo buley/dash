260876032c670329569eb72e5dc96d80e75a3f88
Taylor Buley <taylor.buley@gmail.com>
2014-02-02 21:43:48 -0500

    1st method down, tons to go

--- specs/databases/open.database.spec.js
+++ specs/databases/open.database.spec.js
@@ -1,83 +1,146 @@
 (function(){
 	'use strict';
+	
+	/* Shared expectations across open of new and existing database */
+
+	var beSuccess = function(that) {
+		expect(that.notify).toBe(false);
+		expect(that.error).toBe(false);
+		expect(that.context.error).toBeUndefined();
+		expect(that.success).toBe(true);
+	};
+	
+	var haveReferences = function(that) {
+		expect(that.context.db instanceof IDBDatabase).toBe(true);
+		expect(that.context.request instanceof IDBOpenDBRequest).toBe(true);
+	};
+	
+	var haveNameAndVersion = function(that, dbname) {
+		expect(that.dbname).toBe(that.context.database);
+		expect(that.context.db.name).toBe(that.context.database);
+		expect(that.context.db.version).toBe(1);
+	};
+
 	describe("open.database", function() {
-		var isFinished = false,
-			isFinishedAgain = false,
-			checkIfFinished = function() { 
-				return isFinished;
-			},
-			checkIfFinishedAgain = function() { 
-				return isFinishedAgain;
-			},
-			start_time = new Date().getTime(),
-			db_name = 'store-open-test-' + start_time,
-			error = false,
-			success = false,
-			notify = false,
-			ctx;
+		var start_time = new Date().getTime(),
+			db_name = 'store-open-test-' + start_time;
+		it ('should handle both existing and new databases', function() {
 
-
-		it( 'should open a database', function() {
-			dash.open.database({ database: db_name })
-				.then(function(context) {
-					ctx = context;
-					isFinished = true;
-					success = true;
-				}, function(context) {
-					ctx = context;
-					error = true;
-					isFinished = true;
-				}, function(context) {
-					notify = true;
-				});
-			waitsFor(checkIfFinished, 'the open.database operation to finish', 3000);
-			runs(function() {
-				describe('should finish cleanly', function() {
-					beforeEach(function() {
-						this.context = ctx;
-						this.success = success;
-						this.error = error;
-						this.notify = notify;
-						this.isFinishedAgain = false;
+			/* Test for database creation when opening store that doesn't yet exist */
+			runs(function(){
+				describe( 'new database example', function(){
+					var isFinished = false,
+						dashIsFinished = function() { 
+							return isFinished;
+						},
+						error = false,
+						success = false,
+						notify = false,
+						ctx;	
+					it( 'should open a new database when none exists', function() {
+						dash.open.database({ database: db_name })
+							.then(function(context) {
+								ctx = context;
+								isFinished = true;
+								success = true;
+							}, function(context) {
+								ctx = context;
+								error = true;
+								isFinished = true;
+							}, function(context) {
+								notify = true;
+							});
+						waitsFor(dashIsFinished, 'the open.database operation to finish', 3000);
+						runs(function() {
+							describe('should finish cleanly', function() {
+								beforeEach(function() {
+									this.context = ctx;
+									this.success = success;
+									this.error = error;
+									this.notify = notify;
+									this.dbname = db_name;
+								});
+								it("should be a success", function() {
+									beSuccess(this);
+								});
+								it("should have references to the database and opening request", function() {
+									haveReferences(this);
+								});
+								it('should have the correct attributes', function() {
+									haveNameAndVersion(this);
+								});
+								it('should be an upgrade', function() {
+									expect(this.context.upgrade).toBe(true);
+								});
+								it("should have a context.transaction that's an instanceof IDBTransaction", function() {
+									expect(this.context.transaction instanceof IDBTransaction).toBe(true);
+								});
+							});
+							runs(function(){
+								dash.close.database(ctx);
+							});
+						});
 					});
-					it('should not throw a notify', function() {
-						expect(this.notify).toBe(false);
-					});
-					it('should not throw a error', function() {
-						expect(this.error).toBe(false);
-						expect(this.context.error).toBeUndefined();
-					});
-					it('should throw a success', function() {
-						expect(this.success).toBe(true);
-					});
-					it("should have a context.db that's an instanceof IDBDatabase", function() {
-						console.log(this.context,'instance?',this.context.db instanceof IDBDatabase);
-						expect(this.context.db instanceof IDBDatabase).toBe(true);
-					});
-					it("should have a context.request that's an instanceof IDBOpenDBRequest", function() {
-						expect(this.context.request instanceof IDBOpenDBRequest).toBe(true);
-					});
-					it("should have a context.transaction that's an instanceof IDBTransaction", function() {
-						expect(this.context.transaction instanceof IDBTransaction).toBe(true);
-					});
-					it('should have the correct database name', function() {
-						expect(this.context.db.name).toBe(db_name);
-					});
-					it('should be an upgrade', function() {
-						expect(this.context.upgrade).toBe(true);
-					});
-					it('should have a version of 1', function() {
-						expect(this.context.db.version).toBe(1);
-						this.isFinishedAgain = true;
-					});
-				});
-				runs(function(){
-					console.log('cleanup',ctx);
-					dash.close.database(ctx)
-					.then(dash.remove.database);
 				});
 			});
-		});
 
+			/* Test for opening an existing database: should handle
+			 * in the same way, execept with upgrade flags as false */
+			runs(function(){
+				describe( 'new database example', function(){
+					var isFinished = false,
+						dashIsFinished = function() { 
+							return isFinished;
+						},
+						error = false,
+						success = false,
+						notify = false,
+						ctx;
+					it( 'should open an existing database when one exists', function() {
+						dash.open.database({ database: db_name })
+							.then(function(context) {
+								ctx = context;
+								isFinished = true;
+								success = true;
+							}, function(context) {
+								ctx = context;
+								error = true;
+								isFinished = true;
+							}, function(context) {
+								notify = true;
+							});
+						waitsFor(dashIsFinished, 'the open.database operation to finish', 3000);
+						runs(function() {
+							describe('should finish cleanly', function() {
+								beforeEach(function() {
+									this.context = ctx;
+									this.success = success;
+									this.error = error;
+									this.notify = notify;
+									this.dbname = db_name;
+								});
+								it("should be a success", function() {
+									beSuccess(this);
+								});
+								it("should have references to the database and opening request", function() {
+									haveReferences(this);
+								});
+								it('should have the correct attributes', function() {
+									haveNameAndVersion(this);
+								});
+								it('should not be an upgrade', function() {
+									expect(this.context.upgrade).toBe(false);
+								});
+							});
+							runs(function(){
+								dash.close.database(ctx)
+								.then(dash.remove.database);
+							});
+						});
+					});
+				});
+			});
+			
+		});
 	});
 }());
