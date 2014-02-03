12b5337f2ec746c4267c362264360a918fc829d4
Taylor Buley <taylor.buley@gmail.com>
2014-02-02 22:12:33 -0500

    .

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  var plugins = [];
+  var browsers = [];
+
+  if (process.env.TRAVIS) {
+    plugins.push('karma-firefox-launcher');
+    browsers.push('Firefox');
+  } else {
+    plugins.push('karma-chrome-launcher');
+    browsers.push('Chrome');
+  }
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    karma: {
+      options: {
+        browsers: browsers,
+        files: [
+		  'lib/dash.dev.js',
+          'specs/**/*.js'
+        ],
+        frameworks: ['mocha'],
+        plugins: plugins
+      },
+      continuous: {
+        singleRun: true
+      },
+      dev: {
+        reporters: 'dots',
+        background: true
+      },
+      auto: {
+        autoWatch: true
+      }
+    }
+
+
+  });
+  grunt.loadTasks('tasks');
+  grunt.registerTask('test', ['karma:continuous']);
+  grunt.registerTask('default', ['test']);
+};
---
+++ specs/databases/close.database.spec.js
@@ -0,0 +1,54 @@
+(function(){
+	'use strict';
+	describe("close.database", function() {
+		var start_time = new Date().getTime(),
+			db_name = 'store-close-test-' + start_time,
+			isFinished = false,
+			dashIsFinished = function() { 
+				return isFinished;
+			},
+			error = false,
+			success = false,
+			notify = false,
+			ctx;	
+		it( 'should open and then close', function() {
+			dash.open.database({ database: db_name })
+				.then(dash.close.database)
+				.then(function(context) {
+					ctx = context;
+					isFinished = true;
+					success = true;
+				}, function(context) {
+					ctx = context;
+					error = true;
+					isFinished = true;
+				}, function(context) {
+					notify = true;
+				});
+			waitsFor(dashIsFinished, 'the close.database operation to finish', 3000);
+			runs(function() {
+				describe('database.close should finish cleanly', function() {
+					beforeEach(function() {
+						this.context = ctx;
+						this.success = success;
+						this.error = error;
+						this.notify = notify;
+						this.dbname = db_name;
+					});
+					it("should be a success", function() {
+						expect(this.notify).toBe(false);
+						expect(this.error).toBe(false);
+						expect(this.context.error).toBeUndefined();
+						expect(this.success).toBe(true);
+					});
+					it("should have the correct references", function() {
+						expect(this.context.db instanceof IDBDatabase).toBe(false);
+					});
+				});
+				runs(function(){
+					dash.remove.database(ctx);
+				});
+			});
+		});
+	});
+}());
