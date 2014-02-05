dd5fb932398d511451122e93fa529d90aca3683c
Taylor Buley <taylor.buley@gmail.com>
2014-02-04 22:07:17 -0500

    .

--- specs/stores/add.store.spec.js
+++ specs/stores/add.store.spec.js
@@ -3,24 +3,17 @@
 	describe("add.store", function() {
 		var start_time = new Date().getTime(),
 			db_name = 'store-add-test-' + start_time,
+			store_name = 'store-add-test-store-' + start_time,
 			isFinished = false,
-			isComplete = false,
 			dashIsFinished = function() { 
 				return isFinished;
 			},
-			dashIsComplete = function() {
-				return isComplete;
-			},
 			error = false,
-			seconderror = false,
-			secondsuccess = false,
-			secondnotify = false,
 			success = false,
 			notify = false,
-			prectx,
 			ctx;	
 		it( 'should open a database then add a store', function() {
-			dash.open.database({ database: db_name, store: db_name })
+			dash.open.database({ database: db_name, store: store_name })
 				.then(dash.add.store)
 				.then(function(context) {
 					ctx = context;
@@ -38,11 +31,11 @@
 				describe('add.store should finish cleanly', function() {
 					beforeEach(function() {
 						this.context = ctx;
-						this.setup = prectx;
 						this.success = success;
 						this.error = error;
 						this.notify = notify;
 						this.dbname = db_name;
+						this.storename = store_name;
 					});
 					
 					it("add.store should be a success", function() {
@@ -63,7 +56,7 @@
 
 					it("add.store references should be the db and store we asked for", function(){
 						expect(this.context.db.name).toBe(this.dbname);
-						expect(this.context.objectstore.name).toBe(this.dbname);
+						expect(this.context.objectstore.name).toBe(this.storename);
 					});
 
 					it("add.store should clean up after itself", function() {
---
+++ specs/stores/get.stores.spec.js
@@ -0,0 +1,57 @@
+(function(){
+	'use strict';
+	describe("get.stores", function() {
+		var start_time = new Date().getTime(),
+			db_name = 'stores-get-test-' + start_time,
+			store_name = 'stores-get-test-store-' + start_time,
+			isFinished = false,
+			error = false,
+			success = false,
+			notify = false,
+			ctx;	
+		it( 'should get all stores, when available', function() {
+			dash.open.database({ database: db_name, store: store_name })
+				.then(dash.add.store)
+				.then(dash.get.stores)
+				.then(function(context) {
+					ctx = context;
+					isFinished = true;
+					success = true;
+				}, function(context) {
+					ctx = context;
+					error = true;
+					isFinished = true;
+				}, function(context) {
+					notify = true;
+				});
+			waitsFor(function() { 
+				return isFinished;
+			}, 'the get.stores operation to finish', 3000);
+			runs(function() {
+				describe('get.stores should finish cleanly', function() {
+					beforeEach(function() {
+						this.context = ctx;
+						this.success = success;
+						this.error = error;
+						this.notify = notify;
+					});
+					it("get.stores should be a finish cleanly", function() {
+						expect(this.success).toBe(true);
+						expect(this.error).toBe(false);
+						expect(this.notify).toBe(false);
+					});
+					it("get.stores return a DOMStringList with the right contents", function() {
+						expect(this.context.stores instanceof DOMStringList).toBe(true);
+						expect(this.context.stores.length).toBe(1);
+						expect(this.context.stores.contains(this.context.store)).toBe(true);
+					});
+					it("get.stores should clean up after itself", function() {
+						dash.remove.store(this.context)
+						.then(dash.close.database)
+						.then(dash.remove.database);
+					});
+				});
+			});
+		});
+	});
+}());
