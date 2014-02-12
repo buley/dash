7427ecebf43f58f4354dcd02e78c493af3f283b2
Taylor Buley <taylor.buley@gmail.com>
2014-02-12 00:13:12 -0500

    merge confl

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -38,23 +38,129 @@ dashApp.controller('dashAppAboutController', [ function() {
 dashApp.controller('dashAppDocsController', [ '$scope', '$http', '$templateCache', function( $scope, $http, $templateCache ) {
     console.log('docs controller');
     $scope.documents = [
-        'overview',
-        'databases',
-        'stores'
+        { path: 'overview',
+            title: 'Overview',
+            tags: [ 'general', 'quickstart', 'overview' ],
+            children: [
+                { 'path': 'overview/keys',
+                    'title': 'Keys'
+                },
+                { 'path': 'overview/records',
+                    'title': 'Records'
+                },
+                { 'path': 'overview/requests',
+                    'title': 'Requests'
+                },
+                { 'path': 'overview/security',
+                    'title': 'Security'
+                },
+                { 'path': 'overview/transactions',
+                    'title': 'Transactions'
+                },
+            ]
+        },
+
+        { path: 'indexeddb',
+            title: 'IndexedDB',
+            tags: [ 'indexeddb', 'spec', 'overview', 'html5' ],
+            children: [
+                { 'path': 'indexeddb/cursors',
+                    'title': 'Cursors'
+                },
+                { 'path': 'indexeddb/databases',
+                    'title': 'Databases',
+                    children: [
+                        { 'path': 'indexeddb/database/closing',
+                            'title': 'Closing'
+                        },
+                        { 'path': 'indexeddb/database/removing',
+                            'title': 'Removing'
+                        },
+                        { 'path': 'indexeddb/database/getting',
+                            'title': 'Getting'
+                        },
+                        { 'path': 'indexeddb/database/names',
+                            'title': 'Names'
+                        },
+                        { 'path': 'indexeddb/database/opening',
+                            'title': 'Opening'
+                        },
+                        { 'path': 'indexeddb/database/versionchanges',
+                            'title': 'Version Changes'
+                        },
+                        { 'path': 'indexeddb/database/versions',
+                            'title': 'Versions'
+                        } ]
+                },
+                { 'path': 'indexeddb/entries',
+                    'title': 'Entries',
+                    children: [
+                        { 'path': 'indexeddb/entry/adding',
+                            title: 'Adding' },
+                        { 'path': 'indexeddb/entry/getting',
+                        'title': 'Getting' },
+                        { 'path': 'indexeddb/entry/putting',
+                        'title': 'Putting' },
+                        { 'path': 'indexeddb/entry/removing',
+                        'title': 'Removing' },
+                        { 'path': 'indexeddb/entry/updating',
+                        'title': 'Updating' }
+                    ]
+                },
+                { 'path': 'indexeddb/indexes',
+                    'title': 'Indexes',
+                    'children': [
+                        { 'path': 'indexeddb/index/creating',
+
+                            'title': 'Creating' },
+                        { 'path': 'indexeddb/index/getting',
+                        'title': 'Getting'},
+                        { 'path': 'indexeddb/index/iterating',
+                           'title': 'Iterating' },
+                        { 'path': 'indexeddb/index/removing',
+                        'title': 'Removing'
+                        }
+                    ]
+                },
+                { 'path': 'indexeddb/keyranges',
+                    'title': 'Keyranges',
+                    'children': [
+                        { 'path': 'indexeddb/keyrange/bounds',
+                        'title': 'Bounds' },
+                        { 'path': 'indexeddb/keyrange/direction',
+                        'title': 'Direction' }
+                    ]
+                },
+                { 'path': 'indexeddb/stores',
+                    'title': 'Stores',
+                    children: [
+                        { 'path': 'indexeddb/objectstore/clearing', 'title': 'Clearing' },
+                        { 'path': 'indexeddb/objectstore/creating', 'title': 'Creating' },
+                        { 'path': 'indexeddb/objectstore/getting', 'title': 'Getting' },
+                        { 'path': 'indexeddb/objectstore/iteration', 'title': 'Iterating' },
+                        { 'path': 'indexeddb/objectstore/keypaths', 'title': 'Keypaths' },
+                        { 'path': 'indexeddb/objectstore/removing', 'title': 'Removing' }
+                    ]
+                }
+            ]
+        }
     ];
     _.each( $scope.documents, function(slug) {
         $http.get(['/docs/documents/', slug, '.md' ].join(''), { cache: $templateCache } );
     });
 }]);
+
 dashApp.controller('dashAppDocsContentController', [ function() {
     console.log('content controller');
 }]);
+
 dashApp.controller('dashAppDocsSidebarController', [ function() {
     console.log('sidebar controller');
     _.each( [], function() {
 
     });
 }]);
+
 dashApp.controller('dashAppSplashController', [ function() {
     console.log('splash controller');
 }]);
