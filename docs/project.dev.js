448ed55c2c0235fe5cce78be799e6471d8bffb61
Taylor Buley <taylor.buley@gmail.com>
2014-02-13 07:58:48 -0500

    WIP

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -178,7 +178,7 @@ dashApp.controller('dashAppDocsController', [ '$scope', '$http', '$templateCache
 
 }]);
 
-dashApp.controller('dashAppDocsContentController', [ '$scope', function( $scope ) {
+dashApp.controller('dashAppDocsContentController', [ '$routeParams', '$scope', function( $routeParams, $scope ) {
     console.log('content controller');
     $scope.pathLevel = function(level, slug) {
         console.log('loevel',slug,level, $routeParams['doc'+level]);
