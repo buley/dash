6b6907b85e29892345398d13ac61a78c74c0f64a
Taylor Buley <taylor.buley@gmail.com>
2014-02-13 21:00:56 -0500

    WIP

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -288,6 +288,8 @@ dashApp.controller('dashAppDocsSidebarController', [ '$routeParams', '$scope', f
 
 dashApp.controller('dashAppDocsDemosController', [ '$scope', function( $scope ) {
     console.log('demo controller', $scope.currentDocument());
+    var doc = $scope.currentDocument();
+    $scope.demos = doc.demos;
 }]);
 
 dashApp.controller('dashAppSplashController', [ function() {
