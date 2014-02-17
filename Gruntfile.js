d71fcf198baba3c16d81fc2dbf7e9e9e53fbe2b6
Taylor Buley <taylor.buley@gmail.com>
2014-02-17 15:45:19 -0500

    fixes hanging grunt process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,107 +1,107 @@
 'use strict';
 
-module.exports = function(grunt) {
-  var plugins = [ 'karma-jasmine', 'karma-coverage' ];
-  var browsers = [];
-  if (process.env.TRAVIS) {
-    plugins.push('karma-firefox-launcher');
-    browsers.push('Firefox');
-  } else {
-    plugins.push('karma-chrome-launcher');
-    plugins.push('karma-firefox-launcher');
-    browsers.push('Chrome');
-    browsers.push('Firefox');
-  }
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    karma: {
-      options: {
-	    configFile: 'karma.conf.js'
-      },
-	  prod: {
-	    singleRun: true,
-	    autoWatch: false,
-		browsers: browsers,
-	    plugins: plugins,
-		options: {
-			files: [
-			  'lib/dash.js',
-			  'specs/*.js',
-			  'specs/*/*.js'
-			]
-		},
-	    reporters: [ 'dots', 'coverage' ],
-	    preprocessors: {
-	      "lib/*js": "coverage"
-	    },
-	    coverageReporter: {
-	      type: "lcov",
-	      dir: "coverage/"
-	    },
-	  },
-	  dev: {
-	    singleRun: false,
-	    autoWatch: true,
-		browsers: browsers,
-	    plugins: plugins,
-		options: {
-			files: [
-			  'lib/dash.dev.js',
-			  'specs/*.js',
-			  'specs/*/*.js'
-			]
-		},
-	    reporters: [ 'dots', 'coverage' ],
-	    preprocessors: {
-	      "lib/*.dev.js": "coverage"
-	    },
-	    coverageReporter: {
-	      type: "lcov",
-	      dir: "coverage/"
-	    }
-	  },
-	  chrome: {
-	    singleRun: false,
-	    autoWatch: true,
-		browsers: [ 'Chrome' ],
-	    plugins: plugins,
-		options: {
-			files: [
-			  'lib/dash.dev.js',
-			  'specs/*.js',
-			  'specs/*/*.js'
-			]
-		},
-	    reporters: [ 'dots' ]
-	  },
-	  firefox: {
-	    singleRun: false,
-	    autoWatch: true,
-		browsers: [ 'Firefox' ],
-	    plugins: plugins,
-		options: {
-			files: [
-			  'lib/dash.dev.js',
-			  'specs/*.js',
-			  'specs/*/*.js'
-			]
-		},
-		reporters: [ 'dots' ]
-	  },
-    },
-	coveralls: {
-	    options: {
-		debug: true,
-		coverage_dir: 'coverage/Firefox*'
-	    }
-	}
-  });
-  grunt.loadTasks('tasks');
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-karma-coveralls');
-  grunt.registerTask('prod', ['karma:prod']);
-  grunt.registerTask('dev', ['karma:dev']);
-  grunt.registerTask('firefox', ['karma:firefox']);
-  grunt.registerTask('chrome', ['karma:chrome']);
-  grunt.registerTask('default', ['dev', 'coveralls']);
+module.exports = function (grunt) {
+    var plugins = ['karma-jasmine', 'karma-coverage'];
+    var browsers = [];
+    if (process.env.TRAVIS) {
+        plugins.push('karma-firefox-launcher');
+        browsers.push('Firefox');
+    } else {
+        plugins.push('karma-chrome-launcher');
+        plugins.push('karma-firefox-launcher');
+        browsers.push('Chrome');
+        browsers.push('Firefox');
+    }
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        karma: {
+            options: {
+                configFile: 'karma.conf.js'
+            },
+            prod: {
+                singleRun: true,
+                autoWatch: false,
+                browsers: browsers,
+                plugins: plugins,
+                options: {
+                    files: [
+                        'lib/dash.js',
+                        'specs/*.js',
+                        'specs/*/*.js'
+                    ]
+                },
+                reporters: ['dots', 'coverage'],
+                preprocessors: {
+                    "lib/*js": "coverage"
+                },
+                coverageReporter: {
+                    type: "lcov",
+                    dir: "coverage/"
+                },
+            },
+            dev: {
+                singleRun: true,
+                autoWatch: false,
+                browsers: browsers,
+                plugins: plugins,
+                options: {
+                    files: [
+                        'lib/dash.dev.js',
+                        'specs/*.js',
+                        'specs/*/*.js'
+                    ]
+                },
+                reporters: ['dots', 'coverage'],
+                preprocessors: {
+                    "lib/*.dev.js": "coverage"
+                },
+                coverageReporter: {
+                    type: "lcov",
+                    dir: "coverage/"
+                }
+            },
+            chrome: {
+                singleRun: false,
+                autoWatch: true,
+                browsers: ['Chrome'],
+                plugins: plugins,
+                options: {
+                    files: [
+                        'lib/dash.dev.js',
+                        'specs/*.js',
+                        'specs/*/*.js'
+                    ]
+                },
+                reporters: ['dots']
+            },
+            firefox: {
+                singleRun: false,
+                autoWatch: true,
+                browsers: ['Firefox'],
+                plugins: plugins,
+                options: {
+                    files: [
+                        'lib/dash.dev.js',
+                        'specs/*.js',
+                        'specs/*/*.js'
+                    ]
+                },
+                reporters: ['dots']
+            },
+        },
+        coveralls: {
+            options: {
+                debug: true,
+                coverage_dir: 'coverage/Firefox*'
+            }
+        }
+    });
+    grunt.loadTasks('tasks');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-karma-coveralls');
+    grunt.registerTask('prod', ['karma:prod']);
+    grunt.registerTask('dev', ['karma:dev']);
+    grunt.registerTask('firefox', ['karma:firefox']);
+    grunt.registerTask('chrome', ['karma:chrome']);
+    grunt.registerTask('default', ['dev', 'coveralls']);
 };
