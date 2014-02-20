41d394c781958d345d5f17478121f36084856c1d
Taylor Buley <taylor.buley@gmail.com>
2014-02-19 23:59:36 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -101,6 +101,49 @@ var IMDBSystem = (function(THREE){
             /* What we'll create: a particle system */
             system,
             range,
+            geometry = new THREE.Geometry(),
+            material = new THREE.ParticleBasicMaterial({
+                size: 8,
+                color: 0xFFFFFF,
+                transparent: true,
+                opacity: .6,
+                sizeAttenuation: true,
+                map: (function () {
+                    var texture = new THREE.Texture( (function(height, width, center_x, center_y, radius, points, m, canvas, ctx ) {
+                        var x;
+                        if (!canvas || !ctx) {
+                            if (!canvas) {
+                                canvas = document.createElement('canvas');
+                            }
+                            if (!ctx) {
+                                ctx = canvas.getContext('2d');
+                            }
+                            canvas.height = height;
+                            canvas.width = width;
+                        }
+                        ctx.save();
+                        ctx.beginPath();
+                        ctx.translate(center_x, center_y);
+                        ctx.moveTo(0, (0 - radius));
+                        points = Math.floor( Math.random() * 100 ) % 15;
+                        // super-clever algo via http://programmingthomas.wordpress.com/2012/05/16/drawing-stars-with-html5-canvas/ 
+                        // m = "fraction of radius for inset" 
+                        for ( x = 0; x < points; x += 1) {
+                            ctx.rotate(Math.PI / points);
+                            ctx.lineTo(0, (0 - (radius * m)));
+                            ctx.rotate(Math.PI / points);
+                            ctx.lineTo(0, 0 - radius);
+                        }
+                        ctx.fillStyle = '#C3CBC1';
+                        ctx.fill();
+                        ctx.stroke();
+                        ctx.restore();
+                        return canvas;
+                    }(256, 256, 128, 128, 64, 7, .5)));
+                    texture.needsUpdate = true;
+                    return texture;
+                }())
+            }),
             mouse = { x: 0, y: 0 },
             layout = function() {
                 var test = true;
@@ -114,16 +157,16 @@ var IMDBSystem = (function(THREE){
                 })
                 (function(context) {
                     console.log('dash finished',context);
+                    system = new THREE.ParticleSystem(geometry, material);
+                    system.sortParticles = true;
+                    system.name = "dash-demo";
+                    scene.add(system);
+
                 }, function(context) {
                     console.log('dash error',context);
                 }, function(context) {
-                    var particle = new THREE.Sprite( new THREE.SpriteCanvasMaterial( { color: Math.random() * 0x808080 + 0x808080, program: canvasStarProgram } ) );
-                    particle.position.x = Math.random() * 800 - 400;
-                    particle.position.y = Math.random() * 800 - 400;
-                    particle.position.z = Math.random() * 800 - 400;
-                    particle.name = context.key;
-                    particle.scale.x = particle.scale.y = Math.random() * 20 + 20;
-                    scene.add( particle );
+                    var range = context.entries.length;
+                    geometry.vertices.push(new THREE.Vector3(Math.random() * range - range / 2, Math.random() * range - range / 2, Math.random() * range - range / 2));
                 });
 
             },
