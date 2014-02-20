9a1dd17a4c42451f627a96f5e12f62a0d0d35a47
Taylor Buley <taylor.buley@gmail.com>
2014-02-19 21:32:17 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -48,7 +48,7 @@ var IMDBSystem = (function(THREE){
             scene,
             /* How the viewer sees it */
             camera,
-            projector,
+            projector = new THREE.Projector(),
             /* WebGL vs. Canvas renderer */
             renderer = new THREE.CanvasRenderer(),
             //renderer = new THREE.WebGLRenderer(),
