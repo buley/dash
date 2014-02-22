f6660eb7b7c05d38d2a89e722822e7706d8be470
Taylor Buley <taylor.buley@gmail.com>
2014-02-22 13:41:45 -0500

    WIP

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -168,6 +168,14 @@ var IMDBSystem = (function(THREE){
                 }, function(context) {
                     console.log('dash error',context);
                 }, function(context) {
+
+		    //new THREE.SpriteCanvasMaterial( { color: Math.random() * 0x808080 + 0x808080, program: programStroke } ) );
+		    var particle = new THREE.Sprite( material );
+		    particle.position.x = Math.random() * 800 - 400;
+		    particle.position.y = Math.random() * 800 - 400;
+		    particle.position.z = Math.random() * 800 - 400;
+		    particle.scale.x = particle.scale.y = Math.random() * 20 + 20;
+		    scene.add( particle );
                     geometry.vertices.push(new THREE.Vector3(Math.random() * range - range / 2, Math.random() * range - range / 2, Math.random() * range - range / 2));
                 });
 
