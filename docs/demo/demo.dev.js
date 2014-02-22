6988ba3d8e696698dad6c68101e84233d9aba1a4
Taylor Buley <taylor.buley@gmail.com>
2014-02-22 14:33:29 -0500

    WIP

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -176,6 +176,7 @@ var IMDBSystem = (function(THREE){
 		    for ( x = 0; x < xlen; x += 1 ) {
 		        xitem = entries[ x ];
 		    	var particle = new THREE.Mesh( geometry, material ); 
+			console.log('adidng',xitem);
 		    	particle.position = new THREE.Vector3(xitem.key % 20, xitem.key % 10, 1);
 		    	scene.add( particle );
 
