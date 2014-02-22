2056686e655cd9367103c5791820cd1a39aa90e4
Taylor Buley <taylor.buley@gmail.com>
2014-02-22 17:41:34 -0500

    WIP

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -190,12 +190,14 @@ var IMDBSystem = (function(THREE){
 		    //var particle = new THREE.Sprite( new THREE.SpriteCanvasMaterial( { color: Math.random() * 0x808080 + 0x808080, program: programStroke } ) );
 		    var particle = new THREE.Mesh( geometry, material ); 
 		    //particle.position = new THREE.Vector3(Math.random() * range - range / 2, Math.random() * range - range / 2, Math.random() * range - range / 2);
-		    var x, y, z = 1, slop, size = 5000; //Math.floor( Math.sqrt( context.key ) );
+		    var x, y, z = 1, slop, size = 1000; //Math.floor( Math.sqrt( context.key ) );
 		    x = context.key * 20;
 		    if ( ( x / size ) > 1 ) {
 			var row = Math.floor(x / size);
+			var column = Math.floor(y / size);
 			x = x - (row * size);
 			y = row * 20;
+			z = column * 20;
 		    }
 		    particle.position = new THREE.Vector3(x, y, z);
 		    scene.add( particle );
