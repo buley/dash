c450b26ca0ff8b421a8685a6718d19a2c2b92de9
Taylor Buley <taylor.buley@gmail.com>
2014-02-22 12:46:51 -0500

    WIP

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -197,6 +197,7 @@ var IMDBSystem = (function(THREE){
         return function(node, width, height) {
             range = ( width > height ) ? height : width;
             renderer.setClearColor(0x000000, 1.0);
+            renderer.setSize(width, height);
             node_width = width;
             node_height = height;
             camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
