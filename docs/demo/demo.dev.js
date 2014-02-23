3d7111c93ba44614198906f9b63d709421d13f27
Taylor Buley <taylor.buley@gmail.com>
2014-02-22 19:48:41 -0500

    WIP

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -192,13 +192,13 @@ var IMDBSystem = (function(THREE){
 		    var particle = new THREE.Mesh( geometry, material ); 
 		    //particle.position = new THREE.Vector3(Math.random() * range - range / 2, Math.random() * range - range / 2, Math.random() * range - range / 2);
 		    var x, y, z = 1, slop, size = 200; //Math.floor( Math.sqrt( context.key ) );
-		    x = context.key * 20;
+		    x = context.key * 2;
 		    if ( ( x / size ) > 1 ) {
 			var row = Math.floor(x / size);
 			x = x - (row * size);
-			y = row * 20;
+			y = row * 2;
 			var column = Math.floor(y / size);
-			z = column * 20;
+			z = column * 2;
 			y = y % size;
 		    }
 		    particle.position = new THREE.Vector3(x, y, z);
