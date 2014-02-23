f04a369d5ad635b441bf4331aeddaf35b27f09d5
Taylor Buley <taylor.buley@gmail.com>
2014-02-23 09:58:17 -0500

    WIP

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -251,12 +251,12 @@ var IMDBSystem = (function(THREE){
 	    controls.dynamicDampingFactor = 0.3;
 	    controls.keys = [ 65, 83, 68 ];
 	    controls.addEventListener( 'change', relayout );
-	    light = new THREE.DirectionalLight( 0xffffff );
+	    /*light = new THREE.DirectionalLight( 0xffffff );
 	    light.position.set( 1, 1, 1 );
 	    scene.add( light );
 	    light = new THREE.DirectionalLight( 0x111111 );
 	    light.position.set( -1, -1, -1 );
-	    scene.add( light );
+	    scene.add( light );*/
 	    light = new THREE.AmbientLight( 0x222222 );
 	    scene.add( light );
             node.appendChild(renderer.domElement);
