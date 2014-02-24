12dd2f888925f2a8c6b4e36de8c818e3a934746e
Taylor Buley <taylor.buley@gmail.com>
2014-02-23 19:03:38 -0500

    WIP

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -6,59 +6,9 @@ var IMDBSystem = (function(THREE){
 	    hasStarted = false,
 	    node_width = null,
             node_height = null,
-	    INTERSECTED = null, CHOSEN = null,
-            canvasStarProgram = function(ctx) {
-                var height = 20, width = 20, center_x = 10, center_y = 10, radius = 7, points = 5, m = .5;
-                ctx.save();
-                ctx.beginPath();
-                ctx.strokeStyle = '#ffffff';
-                //ctx.translate(center_x, center_y);
-                ctx.moveTo(0, (0 - radius));
-                //points = Math.floor( Math.random() * 100 ) % 15;
-                // super-clever algo via http://programmingthomas.wordpress.com/2012/05/16/drawing-stars-with-html5-canvas/ 
-                // m = "fraction of radius for inset" 
-                for ( x = 0; x < points; x += 1) {
-                    ctx.rotate(Math.PI / points);
-                    ctx.lineTo(0, (0 - (radius * m)));
-                    ctx.rotate(Math.PI / points);
-                    ctx.lineTo(0, 0 - radius);
-                }
-                ctx.fillStyle = 'transparent';
-                ctx.fill();
-                ctx.stroke();
-                ctx.restore();
-                return ctx;
-            }, canvasFilledStarProgram = function(ctx) {
-                var height = 20, width = 20, center_x = 10, center_y = 10, radius = 7, points = 5, m = .5;
-                ctx.save();
-                ctx.beginPath();
-                //ctx.translate(center_x, center_y);
-                ctx.moveTo(0, (0 - radius));
-                points = Math.floor( Math.random() * 100 ) % 15;
-                ctx.strokeStyle = '#FFFFFF';
-                // super-clever algo via http://programmingthomas.wordpress.com/2012/05/16/drawing-stars-with-html5-canvas/ 
-                // m = "fraction of radius for inset" 
-                for ( x = 0; x < points; x += 1) {
-                    ctx.rotate(Math.PI / points);
-                    ctx.lineTo(0, (0 - (radius * m)));
-                    ctx.rotate(Math.PI / points);
-                    ctx.lineTo(0, 0 - radius);
-                }
-                ctx.fillStyle = '#FFFFFF';
-                ctx.fill();
-                ctx.stroke();
-                ctx.restore();
-                return ctx;
-            }, programFill = function ( ctx ) {
-                ctx.beginPath();
-                ctx.arc( 0, 0, 0.5, 0, PI2, true );
-                ctx.fill();
-            }, programStroke = function ( ctx ) {
-                ctx.lineWidth = 0.025;
-                ctx.beginPath();
-                ctx.arc( 0, 0, 0.5, 0, Math.PI2, true );
-                ctx.stroke();
-            }, render = function() {
+	    INTERSECTED = null,
+	    CHOSEN = null,
+            render = function() {
                 var step = .05;
                 if(camera) {
                     camera.position.y += step;
@@ -223,8 +173,8 @@ var IMDBSystem = (function(THREE){
 	    light.position.set( -1, -1, -1 );
 	    scene.add( light );*/
 
-	    /*light = new THREE.AmbientLight( 0x333333 );
-	    scene.add( light );*/
+	    light = new THREE.AmbientLight( 0x333333 );
+	    scene.add( light );
             node.appendChild(renderer.domElement);
 	    if (stats) {
             	document.getElementsByTagName('body')[0].appendChild( stats.domElement );
