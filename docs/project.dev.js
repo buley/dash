ffa9e6df24a79116311b99c82e7ce01f08e7d468
Taylor Buley <taylor.buley@gmail.com>
2014-02-23 21:33:09 -0500

    WIP

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -372,7 +372,7 @@ dashApp.directive('dashSplash', [ 'dashAppSplashBroadcast', function(dashAppSpla
         compile: function() {
             console.log('dashSplash setup');
             var el = document.createElement('div'),
-                layout = IMDBSystem(el, $('#dash-splash').width(), $('#dash-splash').height(), function(data) {
+                layout/* = IMDBSystem(el, $('#dash-splash').width(), $('#dash-splash').height(), function(data) {
 		    dash.get.entry({
 			database: 'dash-demo',
 			store: 'imdb',
@@ -385,7 +385,7 @@ dashApp.directive('dashSplash', [ 'dashAppSplashBroadcast', function(dashAppSpla
 		    }, function(context) {
 			console.log('missing entry', context);
 		    });
-		});
+		})*/;
             el.setAttribute('id', 'dash-splash-container');
             return function link(scope, element, attrs) {
                 element[0].appendChild(el);
