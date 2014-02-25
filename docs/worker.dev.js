058613bc8e606a0c336180d3edcd7a06753338de
Taylor Buley <taylor.buley@gmail.com>
2014-02-24 21:32:42 -0500

    WIP

--- docs/worker.dev.js
+++ docs/worker.dev.js
@@ -1,10 +1,31 @@
 importScripts('/lib/dash.dev.js');
 self.addEventListener('message', function(e) {
   var input = e.data,
-      output = { start: new Date().getTime(), data: input, dashq: typeof dash };
-  setTimeout(function() {
-    output.end = new Date().getTime();
-    output.lag = output.end - output.start;
-    self.postMessage(output);
-  }, Math.floor(Math.random() * 10000));
+      output = { start: new Date().getTime(), context: input, dashq: typeof dash },
+      method = input.method.split('.'),
+      x = 0,
+      xlen = method.length, curr = dash, thou = self;
+  for ( x = 0; x < xlen; x += 1 ) {
+	if ( undefined !== dash[ method[ x ] ) {
+		curr = curr[ method[ x ] ];
+	}
+  }
+  var end = function(ctx) {
+	output.context = ctx;
+	output.end = new Date().getTime();
+	output.lag = output.end - output.start;
+	thou.postMessage(output);
+  };
+  curr( input.context )(
+	function(context) {
+		output.type = 'success';
+		end(context);	
+	}, function(context) {
+		output.type = 'error';
+		end(context);	
+	}, function(context) {
+		output.type = 'notify';
+		end(context);	
+	},
+  );
 }, false);
