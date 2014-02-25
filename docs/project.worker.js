89568e1c6c0d724aad8399719a56e91f277fd8b1
Taylor Buley <taylor.buley@gmail.com>
2014-02-24 23:26:33 -0500

    .

--- docs/project.worker.js
+++ docs/project.worker.js
@@ -1,30 +1,7 @@
 if (self) {
   importScripts('/lib/dash.dev.js');
   self.addEventListener('message', function(e) {
-	  var input = e.data,
-	      method = input.dash.split('.'),
-	      verb = method[ 0 ],
-              noun = method[ 1 ];
-	  var end = function(ctx) {
-		input.context = ctx;
-		thou.postMessage(input);
-	  };
-          if ( undefined !== dash[ verb ] && undefined !== dash[ verb ][ noun ] ) {
-		  dash[ verb ][ noun ]( input.context )(
-			function(context) {
-				input.type = 'success';
-				end(context);	
-			}, function(context) {
-				input.type = 'error';
-				end(context);	
-			}, function(context) {
-				input.type = 'notify';
-				end(context);	
-			}
-		  );
-	} else {
-		input.type = 'error';
-		end( { error: 'No such method' } );
-	}
+	  var input = e.data;
+          self.postMessage(input);
   }, false);
 }
