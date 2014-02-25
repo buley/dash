eb9cbc4dfaa1d83fe1861bbe3960c724f9486ada
Taylor Buley <taylor.buley@gmail.com>
2014-02-24 21:38:23 -0500

    WIP

--- docs/worker.dev.js
+++ docs/worker.dev.js
@@ -4,10 +4,12 @@ self.addEventListener('message', function(e) {
       output = { start: new Date().getTime(), context: input, dashq: typeof dash },
       method = input.method.split('.'),
       x = 0,
-      xlen = method.length, curr = dash, thou = self;
+      xlen = method.length, curr = dash, thou = self, error = false;
   for ( x = 0; x < xlen; x += 1 ) {
 	if ( undefined !== dash[ method[ x ] ] ) {
 		curr = curr[ method[ x ] ];
+	} else { 
+		error = true;
 	}
   }
   var end = function(ctx) {
@@ -16,16 +18,21 @@ self.addEventListener('message', function(e) {
 	output.lag = output.end - output.start;
 	thou.postMessage(output);
   };
-  curr( input.context )(
-	function(context) {
-		output.type = 'success';
-		end(context);	
-	}, function(context) {
-		output.type = 'error';
-		end(context);	
-	}, function(context) {
-		output.type = 'notify';
-		end(context);	
-	}
-  );
+  if ( error ) {
+        output.type = 'error';
+ 	end(null);
+  } else { 
+	  curr( input.context )(
+		function(context) {
+			output.type = 'success';
+			end(context);	
+		}, function(context) {
+			output.type = 'error';
+			end(context);	
+		}, function(context) {
+			output.type = 'notify';
+			end(context);	
+		}
+	  );
+  } 
 }, false);
