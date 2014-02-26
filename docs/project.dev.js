c2974546c1941aef9c56fa995c41bd82f7adb79e
Taylor Buley <taylor.buley@gmail.com>
2014-02-25 19:43:03 -0500

    WIP

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -716,10 +716,8 @@ dashApp.directive('dashSplashOverlay', [ '$q', '$http', 'dashAppSplashBroadcast'
 				data: { foo: 'bar' }
 			    }
 			});
-			console.log('started worer');
 			var x = 0, xlen = values.length;
 			for ( x = 0; x < xlen; x += 1 ) {
-				console.log("VAL",values[x]);
 				if ( true === values[ x ][ 1 ] ) {
 					promise = promise.then( (function(attr) {
 						var deferred2 = $q.defer();
