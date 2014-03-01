cf9bb1ce728e4220a89996e0970660ceba71cdd7
Taylor Buley <taylor.buley@gmail.com>
2014-03-01 15:58:49 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -127,9 +127,9 @@ var IMDBSystem = (function(THREE){
 		    light.position.set( 1, 1, 1 );
 		    scene.add( light );*/
 
-		    /*light = new THREE.DirectionalLight( 0x111111 );
+		    light = new THREE.DirectionalLight( 0x111111 );
 		    light.position.set( -1, -1, -1 );
-		    scene.add( light );*/
+		    scene.add( light );
 
 		    light = new THREE.AmbientLight( 0x111111 );
 		    scene.add( light );
