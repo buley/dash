3b66051fe0bae6cd9c47691c560fbf2a6ecc6629
Taylor Buley <taylor.buley@gmail.com>
2014-03-01 00:33:16 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -87,7 +87,7 @@ var IMDBSystem = (function(THREE){
             range,
 	    stats,
 	    geometry = new THREE.SphereGeometry(3, 32, 32), 
-            material = new THREE.MeshBasicMaterial({color: new THREE.Color( 0xFFFFFF ), sizeAttenuation: true }),
+            material = new THREE.MeshLambertMaterial({color: new THREE.Color( 0xFFFFFF ), sizeAttenuation: true }),
             mouse = { x: 0, y: 0 },
             /* When properties change we'll need a re-layout */
             onMouseMove = function(event) {
