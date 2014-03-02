4a21a21484dce99a04bad46fc2ecd6c65afe47e9
Taylor Buley <taylor.buley@gmail.com>
2014-03-01 20:25:11 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -79,7 +79,7 @@ var IMDBSystem = (function(THREE){
             /* What we'll create: a particle system */
             range,
 	    stats,
-	    geometry = new THREE.SphereGeometry(16, 256, 256),
+	    geometry = new THREE.SphereGeometry(16, 24, 24),
             material = new THREE.MeshPhongMaterial({color: 'dark gray', sizeAttenuation: true }),
             mouse = { x: 0, y: 0 },
             /* When properties change we'll need a re-layout */
