c82831abb5c343f538d59c7e537838d586a4868f
Taylor Buley <taylor.buley@gmail.com>
2014-03-02 15:54:57 -0500

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -789,7 +789,7 @@ dashApp.directive('dashSplashOverlay', [ '$q', '$http', 'dashAppSplashBroadcast'
 				console.log('dash promise rejected', context);
 			    }, function(context) {
 				system.add(context.entry);
-				system.cameraMod( 'z', -1, 10000, 0 );
+				system.cameraMod( 'z', -1, 10000, 1000 );
 				//system.cameraMod( 'z', 1, 10000, 0 );
 			    });
 
