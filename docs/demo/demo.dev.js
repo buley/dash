a40ebb2a53b07374231e64f75f12975a7c79aefd
Taylor Buley <taylor.buley@gmail.com>
2014-03-01 21:05:50 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -21,9 +21,9 @@ var IMDBSystem = (function(THREE){
                 stats.update();
 		controls.update();
 		camera.updateMatrixWorld();
-		var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+		//var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
 		projector.unprojectVector( vector, camera );
- 		raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+ 		raycaster = new THREE.Raycaster( vector, vector.sub( camera.position ).normalize() );
 		var intersects = raycaster.intersectObjects( scene.children );
 		if ( intersects.length > 0 ) {
 		    if ( INTERSECTED != intersects[ 0 ].object ) {
