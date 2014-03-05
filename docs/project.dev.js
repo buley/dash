79d367b3b0a5246e4dfa85b1fc1ec606b98573e6
Taylor Buley <taylor.buley@gmail.com>
2014-03-04 23:07:09 -0500

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -826,7 +826,9 @@ dashApp.directive('dashSplashOverlay', [ '$q', '$http', 'dashAppSplashBroadcast'
 				}
 			}
 			if ( is_installed ) {
-				xlen = scope.fields.length, field, total = totalDownloaded(scope.downloaded, scope.range, scope.sort );
+				xlen = scope.fields.length;
+				var field,
+				    total = totalDownloaded(scope.downloaded, scope.range, scope.sort );
 				for ( x = 0; x < xlen; x += 1 ) {
 					field = scope.fields[ x ].name;
 					if ( 'million' === field && total < 1000000) {
