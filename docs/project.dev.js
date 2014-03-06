a558ade3bb94517d5af33ae99c83bfb43b23c782
Taylor Buley <taylor.buley@gmail.com>
2014-03-05 20:28:19 -0500

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -939,6 +939,8 @@ dashApp.directive('dashSplashOverlay', [ '$q', '$http', 'dashAppSplashBroadcast'
 				if ( 'complete' === tag ) {
 					statsObj ={ verb: arguments[1], complete: true, amount: arguments[2], elapsed: arguments[3] };
 					scope.statsData = statsObj;
+					clear( statsProc );
+					return;
 				} else {
 					statsObj[ tag ] = statsObj[ tag ] || 0;
 					statsObj[ tag ] += 1;
