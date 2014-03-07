cb5bafa4f9ece700a556432aeffe798463ddb8bc
Taylor Buley <taylor.buley@gmail.com>
2014-03-06 23:50:28 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -297,6 +297,7 @@ var IMDBSystem = (function(THREE){
 				controls = new THREE.TrackballControls( camera );
 				pointerlockctls = new THREE.PointerLockControls( camera );
 				scene.add( controls );
+				scene.add( pointerlockctls );
 
 
 			//End pointer lock
