93859305690356f433c891e258c63090cfa3f79c
Taylor Buley <taylor.buley@gmail.com>
2014-03-06 21:21:17 -0500

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -497,7 +497,7 @@ dashApp.directive('dashSplashOverlay', [ '$q', '$http', 'dashAppSplashBroadcast'
 				prev_update = last_updated;
 				last_updated = new Date().getTime();
 				var pretty = function(rate) {
-					var quant = (rate + 1)/(new Date().getTime() - last_updated) + 1;
+					var quant = (rate + 1)/(last_updated - prev_update) + 1;
 					/*if ( quant < 1 ) {
 						return Math.floor(rate/(scope.statsData.elapsed/60000)) + ' entries per minute';
 					}*/
