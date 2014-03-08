5b8cdbf6dc7a118586e73e74ee782a3447afa7dc
Taylor Buley <taylor.buley@gmail.com>
2014-03-08 17:37:56 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -104,7 +104,7 @@ var IMDBSystem = (function(THREE){
 		overdraw: true,
                 color: 0xFFFFFF,
                 transparent: true,
-                opacity: 1,
+                opacity: .7,
                 sizeAttenuation: true,
 		side: THREE.DoubleSide,
                 map: (function () {
