e57fa2f16ff9246d0830f5a3db52f339e7f1b6e5
Taylor Buley <taylor.buley@gmail.com>
2014-03-07 20:16:20 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -188,6 +188,7 @@ var IMDBSystem = (function(THREE){
                 //event.preventDefault();
                 mouse.x = ( event.clientX / node_width ) * 2 - 1;
                 mouse.y = - ( event.clientY / node_height ) * 2 + 1;
+		console.log(mouse);
             },
             onResize = function(event) {
                 camera.aspect = node_width / node_height;
