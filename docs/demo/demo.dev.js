db7036172764123e2140508ea299979399d9369b
Taylor Buley <taylor.buley@gmail.com>
2014-03-07 21:11:55 -0500

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -79,7 +79,7 @@ var IMDBSystem = (function(THREE){
 				console.log("OLD OBJ",obj.object);
 				obj = intersects[ inc ]; 
 			}
-			console.log("OBJ",intersects.length, obj);
+			console.log("OBJ",intersects.length, intersects[1], obj);
 		    if ( INTERSECTED != obj ) {
 			//if ( INTERSECTED ) INTERSECTED.material.program = canvasFilledStarProgram;
 			INTERSECTED = obj;
