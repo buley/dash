69da9f9e1358b5dd379f3fef5e6b43a95fb7bbe9
Taylor Buley <taylor.buley@gmail.com>
2014-03-07 22:40:09 -0500

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -457,10 +457,10 @@ dashApp.directive('dashSplashOverlay', [ '$q', '$http', '$timeout', 'dashAppSpla
             el.setAttribute('id', 'dash-splash-container');
             return function link(scope, element, attrs) {
                 var system = IMDBSystem(el, $('#dash-splash-overlay').width(), $('#dash-splash-overlay').height(), function(data) {
+		    if (pid) {
+			clearTimeout(pid);
+		    }
 		    if (!data) {
-			if (pid) {
-				clearTimeout(pid);
-			}
 			pid = setTimeout( function() {
 			scope.$apply(function() {
 				scope.data = {
