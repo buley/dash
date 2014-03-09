b6616d14a37d5a8812f49d271c21697986a6822a
Taylor Buley <taylor.buley@gmail.com>
2014-03-09 17:27:59 -0400

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -598,7 +598,7 @@ dashApp.directive('dashSplashOverlay', [ '$q', '$http', '$timeout', 'dashAppSpla
 						scope.statsDisplay.avgRate = avg/x;
 						scope.statsDisplay.secondsElapsed = Math.floor(scope.statsData.started/1000);
 						var remainder = (scope.statsData.stack.total - scope.statsData.stack.progress);
-						scope.statsDisplay.secondsRemain = Math.floor(remainder/scope.statsDisplay.avgRate);
+						scope.statsDisplay.secondsRemain = Math.floor((remainder/scope.statsDisplay.avgRate));
 						scope.statsDisplay.prettyTime = prettyTime( scope.statsDisplay.secondsRemain );
 						scope.statsDisplay.prettyElapsed = prettyTime(scope.statsDisplay.secondsElapsed);
 
