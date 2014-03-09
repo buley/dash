6a5429fc41bdd7c597ff8aaf1e41ab45a62320e9
Taylor Buley <taylor.buley@gmail.com>
2014-03-08 22:26:10 -0500

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -501,11 +501,11 @@ dashApp.directive('dashSplashOverlay', [ '$q', '$http', '$timeout', 'dashAppSpla
 					var quant = (rate/scope.statsData.elapsed) * 1000;
 					if ( quant < 1 ) {
 						rate = Math.floor(quant * 60);
-						console.log('stack1',((scope.statsData.stack.total - scope.statsData.stack.progress) /rate));
+						console.log('stack1',Math.floor((scope.statsData.stack.total - scope.statsData.stack.progress) /rate));
 						return rate + ' entries per minute';
 					}
 					rate = Math.floor(quant);
-					console.log('stack2',((scope.statsData.stack.total - scope.statsData.stack.progress) /rate));
+					console.log('stack2',Math.floor((scope.statsData.stack.total - scope.statsData.stack.progress) /rate));
 					return rate + ' entries per second';
 					
 				};
