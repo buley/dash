641a5d817bfb1f4f4496a1a465d2dfb973d0b342
Taylor Buley <taylor.buley@gmail.com>
2014-03-09 20:51:44 -0400

    .

--- docs/demo/demo.dev.js
+++ docs/demo/demo.dev.js
@@ -335,12 +335,12 @@ var IMDBSystem = (function(THREE){
 	            bounding1 = new THREE.CubeGeometry( range, range, range, 8,8,8 );
 		    bounding_material1 = new THREE.MeshBasicMaterial( {color: 0x333333, wireframe: false, transparent: true, opacity: .2 } ),
 		    cube1 = new THREE.Mesh( bounding1, bounding_material1 );
-		    cube1.rotation.z = (Math.PI/180) * 90;
+		    cube1.rotation.x = (Math.PI/180) * 90;
 		    scene.add(cube1);
 	            bounding2 = new THREE.CubeGeometry( range + 100, range + 100, range + 100, 2, 2, 2 );
 		    bounding_material2 = new THREE.MeshBasicMaterial( {color: 0x222222, wireframe: true, transparent: true, opacity: .5 } );
 		    cube2 = new THREE.Mesh( bounding2, bounding_material2 );
-		    cube2.rotation.z = (Math.PI/180) * 90;
+		    cube2.rotation.x = (Math.PI/180) * 90;
 		    scene.add(cube2);
 
 		    node.appendChild(renderer.domElement);
