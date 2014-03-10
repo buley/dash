5da899eb2deca0805a7eb351e7d642af705e6880
Taylor Buley <taylor.buley@gmail.com>
2014-03-09 21:50:49 -0400

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -932,6 +932,9 @@ dashApp.directive('dashSplashOverlay', [ '$q', '$http', '$timeout', 'dashAppSpla
 			}
 			return downloaded;
 		};
+		scope.$watch( 'visualization', function(newer, older) {
+			system.reset();
+		} );
 		scope.$watch( 'range', function(newer, older) {
 			var is_installed, x, xlen = scope.verbs.length;
 			if ( true === hasDownloaded(newer) ) {
