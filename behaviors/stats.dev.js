d87247a8ced2c5ddf8e2f6a7a9a497af8015d8d0
Taylor Buley <taylor.buley@gmail.com>
2014-03-11 23:30:29 -0400

    .

--- behaviors/stats.dev.js
+++ behaviors/stats.dev.js
@@ -2,8 +2,8 @@ window.dashStats = window.dashStats || (function(w) {
 	"use strict";
 	return function(ctx) {
 		var promise = ctx.promise,
-		    deferred = this.promise();
-		promise( function( state ) {
+		    deferred = ctx.deferred;
+		deferred( function( state ) {
 			setTimeout( function() {
 				console.log('module before and after callback', state);
 				deferred.resolve(state);
