aa3b0ecf82debaf25775bc11aaa6d1ee6879ab2b
Taylor Buley <taylor.buley@gmail.com>
2014-03-14 21:50:43 -0400

    .

--- behaviors/stats.dev.js
+++ behaviors/stats.dev.js
@@ -2,11 +2,6 @@ window.dashStats = window.dashStats || (function(environment) {
 	"use strict";
 	var model = function() {
 	    return {
-		request: {
-			start: NaN,
-			complete: NaN,
-			
-		},
 		requests: {
 			add: 0,
 			clears: 0,
@@ -28,8 +23,11 @@ window.dashStats = window.dashStats || (function(environment) {
 			database: 0,
 			databases: 0
 		},
-		resolves: 0,
-		notifies: 0,
+		outcomes: {
+			resolve: 0,
+			notify: 0,
+			error: 0
+		},
 		milliseconds: {
 			start: NaN,
 			elapsed: NaN,
@@ -38,58 +36,75 @@ window.dashStats = window.dashStats || (function(environment) {
 		display: {
 			elapsed: '',
 			remaining: ''
-		}
+		},
 		metrics: {
-			adds: {
+			add: {
 				average: NaN,
 				rate: NaN,
 				recent: []
 			},
-			clears: {
+			clear: {
 				average: NaN,
 				rate: NaN,
 				recent: []
 			},
-			counts: {
+			count: {
 				average: NaN,
 				rate: NaN,
 				recent: []
 			},
-			gets: {
+			get: {
 				average: NaN,
 				rate: NaN,
 				recent: []
 			},
-			puts: {
+			put: {
 				average: NaN,
 				rate: NaN,
 				recent: []
 			},
-			removes: {
+			remove: {
 				average: NaN,
 				rate: NaN,
 				recent: []
 			},
-			updates: {
+			update: {
 				average: NaN,
 				rate: NaN,
 				recent: []
 			}
 		},
+		type: null,
 		dom: {
 			node: null
 		}
 	    };
 	},
+	recents = 5,
 	total = model();
 	return function(state) {
 		var context = state.context;
 		if ( !context.stats_start ) {
 			console.log('starting',state.type);
-			state.context.stats_start = new Date().getTime();
-			state.context.stats_operation = state.type;
+
+			stats.context.statistics = model();
+			state.context.statistics.milliseconds.start = new Date().getTime();
+			state.context.statistics.type = state.type;
 		} else {
-			console.log('finished', context.stats_operation, new Date().getTime() - context.stats_start );
+			state.context.statistics.milliseconds.finished = new Date().getTime();
+			state.context.statistics.milliseconds.elapsed = state.context.statistics.milliseconds.finished - state.context.statistics.milliseconds.started;
+			var pieces = state.context.statistics.type.split('.'),
+			    verb = pieces[ 0 ],
+			    noun = pieces[ 1 ];
+			state.context.statistics.outcomes[ state.type ] += 1;
+			state.context.statistics.requests[ verb ] += 1;
+			state.context.statistics.targets[ noun ] += 1;
+			state.context.statistics.metrics[ verb ].recent.unshift(state.contex.statistics.milliseconds.elapsed);
+			if ( state.context.statistics.metrics[ verb ].recent.length > recents ) {
+				state.context.statistics.metrics[ verb ].recent = state.context.statistics.metrics[ verb ].recent.slice(0, 5);
+				
+			}	
+			console.log('stats', state.contet.statistics);
 		}
 		return state;
 	};
