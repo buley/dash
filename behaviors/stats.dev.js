b93b55d9e2a0da0de4abe1c0dd00f01e1070392d
Taylor Buley <taylor.buley@gmail.com>
2014-03-14 20:53:02 -0400

    .

--- behaviors/stats.dev.js
+++ behaviors/stats.dev.js
@@ -4,7 +4,7 @@ window.dashStats = window.dashStats || (function(environment) {
 		var context = state.context;
 		if ( !context.stats_start ) {
 			state.context.stats_start = new Date().getTime();
-			stats.context.stats_operation = stats.type;
+			stats.context.stats_operation = state.type;
 		} else {
 			console.log('finished', context.stats_operation, new Date().getTime() - context.stats_start );
 		}
