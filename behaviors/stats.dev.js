57c507e094d5abe7e5a25986f4f4ba3d6c84afb1
Taylor Buley <taylor.buley@gmail.com>
2014-03-14 21:39:25 -0400

    .

--- behaviors/stats.dev.js
+++ behaviors/stats.dev.js
@@ -1,21 +1,87 @@
 window.dashStats = window.dashStats || (function(environment) {
 	"use strict";
-	var cache = {
-		total: {
-			requests: {
-				add: 0,
-				clears: 0,
-				counts: 0,
-				get: 0,
-				put: 0,
-				remove: 0,
-				update: 0
-			},
-			resolves: 0,
-			notifies: 0,
-			errors: 0
+	var model = function() {
+	    return {
+		request: {
+			start: NaN,
+			complete: NaN,
+			
+		},
+		requests: {
+			add: 0,
+			clears: 0,
+			counts: 0,
+			get: 0,
+			put: 0,
+			remove: 0,
+			update: 0
+		},
+		targets: {
+			attribute: 0,
+			behavior: 0,
+			store: 0,
+			stores: 0,
+			entry: 0,
+			entries: 0,
+			index: 0,
+			indexes: 0,
+			database: 0,
+			databases: 0
+		},
+		resolves: 0,
+		notifies: 0,
+		milliseconds: {
+			start: NaN,
+			elapsed: NaN,
+			remaining: NaN
+		},
+		display: {
+			elapsed: '',
+			remaining: ''
 		}
-	};
+		metrics: {
+			adds: {
+				average: NaN,
+				rate: NaN,
+				recent: []
+			},
+			clears: {
+				average: NaN,
+				rate: NaN,
+				recent: []
+			},
+			counts: {
+				average: NaN,
+				rate: NaN,
+				recent: []
+			},
+			gets: {
+				average: NaN,
+				rate: NaN,
+				recent: []
+			},
+			puts: {
+				average: NaN,
+				rate: NaN,
+				recent: []
+			},
+			removes: {
+				average: NaN,
+				rate: NaN,
+				recent: []
+			},
+			updates: {
+				average: NaN,
+				rate: NaN,
+				recent: []
+			}
+		},
+		dom: {
+			node: null
+		}
+	    };
+	},
+	total = model();
 	return function(state) {
 		var context = state.context;
 		if ( !context.stats_start ) {
