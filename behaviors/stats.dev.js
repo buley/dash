4663657037dfe3a7ecc3d9d75c46e65b4b7c2847
Taylor Buley <taylor.buley@gmail.com>
2014-03-15 19:36:03 -0400

    .

--- behaviors/stats.dev.js
+++ behaviors/stats.dev.js
@@ -245,6 +245,12 @@ window.dashStats = window.dashStats || (function (environment) {
 
     }
     console.log('stats', state.context.statistics);
+    state.context.statistics.request.remaining[verb] = state.context.statistics.request.expected[verb] - state.context.statistics.request.requests[verb];
+    state.context.statistics.total.remaining[verb] = state.context.statistics.total.expected[verb] - state.context.statistics.total.requests[verb];
+    state.context.statistics.request.remaining[noun] = state.context.statistics.request.expected[noun] - state.context.statistics.request.requests[noun];
+    state.context.statistics.total.remaining[noun] = state.context.statistics.total.expected[noun] - state.context.statistics.total.requests[noun];
+    state.context.statistics.request.remaining.total = state.context.statistics.request.expected.total - state.context.statistics.request.requests.total;
+    state.context.statistics.total.remaining.total = state.context.statistics.total.expected.total - state.context.statistics.total.requests.total;
     return state;
   };
 }(self));
