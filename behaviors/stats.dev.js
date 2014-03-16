c29feeec953131daad0d91a3d07dd990efce0929
Taylor Buley <taylor.buley@gmail.com>
2014-03-15 22:52:31 -0400

    .

--- behaviors/stats.dev.js
+++ behaviors/stats.dev.js
@@ -278,7 +278,7 @@ window.dashStats = window.dashStats || (function (environment) {
         state.context.statistics.total.prettyRemaining[v] = prettyTime(state.context.statistics.total.remaining[v]);
         state.context.statistics.total.prettyRemaining[n] = prettyTime(state.context.statistics.total.remaining[n]);
         state.context.statistics.total.prettyRemaining.total = prettyTime(state.context.statistics.total.remaining.total);
-        console.log('elapsed',state.context.statistics.total.prettyElapsed,);
+        console.log('elapsed',state.context.statistics.total.prettyElapsed);
         console.log('duration',state.context.statistics.total.prettyDuration, state.context.statistics.total.prettyRemaining);
       };
     state.context.statistics = state.context.statistics || {
