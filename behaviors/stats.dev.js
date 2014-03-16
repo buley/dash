b5d3088e560359994d5aa87f86024c463377e2f0
Taylor Buley <taylor.buley@gmail.com>
2014-03-15 20:24:58 -0400

    .

--- behaviors/stats.dev.js
+++ behaviors/stats.dev.js
@@ -210,6 +210,12 @@ window.dashStats = window.dashStats || (function (environment) {
             state.context.statistics.request.expected.total += ctx.total;
             state.context.statistics.total.expected.total += ctx.total;
           }
+          state.context.statistics.request.expected[verb] += 1;
+          state.context.statistics.request.expected[noun] += 1;
+          state.context.statistics.total.expected[verb] += 1;
+          state.context.statistics.total.expected[noun] += 1;
+          state.context.statistics.total.expected.total += 1;
+          state.context.statistics.total.expected.total += 1;
           state.context.statistics.request.remaining[verb] = state.context.statistics.request.expected[verb] - state.context.statistics.request.requests[verb];
           state.context.statistics.total.remaining[verb] = state.context.statistics.total.expected[verb] - state.context.statistics.total.requests[verb];
           state.context.statistics.request.remaining[noun] = state.context.statistics.request.expected[noun] - state.context.statistics.request.requests[noun];
