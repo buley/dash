9491da7dfdbadf570402b329e1ffb612837ec8a6
Taylor Buley <taylor.buley@gmail.com>
2014-03-16 21:27:03 -0400

    .

--- behaviors/stats.dev.js
+++ behaviors/stats.dev.js
@@ -351,13 +351,13 @@ window.dashStats = window.dashStats || (function (environment) {
         state.context.statistics.total.duration[n] = state.context.statistics.total.metrics[n].average * state.context.statistics.total.expected[n];
         state.context.statistics.total.duration.total = state.context.statistics.total.metrics.total.average * state.context.statistics.total.expected.total;
         
-        state.context.statistics.total.remaining[v] = state.context.statistics.total.expecting[v] * state.context.statistics.total.metrics[v].rate;
-        state.context.statistics.total.remaining[n] = state.context.statistics.total.expecting[n] * state.context.statistics.total.metrics[n].rate;
-        state.context.statistics.total.remaining.total = state.context.statistics.total.expecting.total * state.context.statistics.total.metrics.total.rate;
+        state.context.statistics.total.remaining[v] = state.context.statistics.total.expecting[v] * state.context.statistics.total.metrics[v].average;
+        state.context.statistics.total.remaining[n] = state.context.statistics.total.expecting[n] * state.context.statistics.total.metrics[n].average;
+        state.context.statistics.total.remaining.total = state.context.statistics.total.expecting.total * state.context.statistics.total.metrics.total.average;
 
-        state.context.statistics.request.remaining[v] = state.context.statistics.request.expecting[v] * state.context.statistics.request.metrics[v].rate
-        state.context.statistics.request.remaining[n] = state.context.statistics.request.expecting[n] * state.context.statistics.request.metrics[n].rate;
-        state.context.statistics.request.remaining.total = state.context.statistics.request.expecting.total * state.context.statistics.request.metrics.total.rate;
+        state.context.statistics.request.remaining[v] = state.context.statistics.request.expecting[v] * state.context.statistics.request.metrics[v].average
+        state.context.statistics.request.remaining[n] = state.context.statistics.request.expecting[n] * state.context.statistics.request.metrics[n].average;
+        state.context.statistics.request.remaining.total = state.context.statistics.request.expecting.total * state.context.statistics.request.metrics.total.average;
         if (0 > state.context.statistics.total.remaining[v]) {
           state.context.statistics.total.remaining[v] = 0;
         } 
