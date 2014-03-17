8c13ba239b876d026d7fc78e55d99735ceefa908
Taylor Buley <taylor.buley@gmail.com>
2014-03-16 21:59:03 -0400

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -1606,7 +1606,7 @@ dashApp.directive('dashSplashOverlay', ['$q', '$http', '$timeout', 'dashAppSplas
                 scope.statsDisplay.complete = statsObj.request.requests.total;
                 scope.statsDisplay.prettyElapsed = statsObj.request.prettyActual.total
                 scope.statsDisplay.prettyRemain = statsObj.request.prettyRemaining.total;
-                scope.statsDisplay.prettyAvg = Math.floor(1000/statsObj.request.metrics.total.average) + 'entries/sec';
+                scope.statsDisplay.prettyAvg = Math.floor(1000/statsObj.request.metrics.total.average) + ' entries/sec';
                 scope.statsDisplay.prettyRate = Math.floor(1000/statsObj.request.metrics.total.rate) + ' entries/sec';        
               }
               statsUIProc = null;
