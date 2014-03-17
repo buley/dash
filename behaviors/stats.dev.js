6db20172ee879d28ad5a11b4f82f5a129baefccc
Taylor Buley <taylor.buley@gmail.com>
2014-03-16 22:18:35 -0400

    .

--- behaviors/stats.dev.js
+++ behaviors/stats.dev.js
@@ -481,17 +481,17 @@ window.dashStats = window.dashStats || (function (environment) {
         if ( state.context.statistics.total.prettySpeedRate[v] < 1 ) {
           state.context.statistics.total.prettySpeedRate[v] = Math.floor(60/state.context.statistics.total.prettySpeedRate[v]) + ' ms/entry';
         } else {
-          state.context.statistics.total.prettySpeedRate[v] += ' entries/sec';
+          state.context.statistics.total.prettySpeedRate[v] += ' ms/entry';
         }
         if ( state.context.statistics.total.prettySpeedRate[n] < 1 ) {
           state.context.statistics.total.prettySpeedRate[n] = Math.floor(60/state.context.statistics.total.prettySpeedRate[n]) + ' ms/entry';
         } else {
-          state.context.statistics.total.prettySpeedRate[n] += ' entries/sec';
+          state.context.statistics.total.prettySpeedRate[n] += ' ms/entry';
         } 
         if ( state.context.statistics.total.prettySpeedRate.total < 1 ) {
           state.context.statistics.total.prettySpeedRate.total = Math.floor(60/state.context.statistics.total.prettySpeedRate.total) + ' ms/entry';
         } else {
-          state.context.statistics.total.prettySpeedRate.total += ' entries/sec';
+          state.context.statistics.total.prettySpeedRate.total += ' ms/entry';
         } 
         state.context.statistics.total.prettySpeedAverage[v] = Math.floor(state.context.statistics.total.metrics[v].average);
         state.context.statistics.total.prettySpeedAverage[n] = Math.floor(state.context.statistics.total.metrics[n].average);
@@ -499,17 +499,17 @@ window.dashStats = window.dashStats || (function (environment) {
         if ( state.context.statistics.total.prettySpeedAverage[v] < 1 ) {
           state.context.statistics.total.prettySpeedAverage[v] = Math.floor(60/state.context.statistics.total.prettySpeedAverage[v]) + ' ms/entry';
         } else {
-          state.context.statistics.total.prettySpeedAverage[v] += ' entries/sec';
+          state.context.statistics.total.prettySpeedAverage[v] += ' ms/entry';
         }       
         if ( state.context.statistics.total.prettySpeedAverage[n] < 1 ) {
           state.context.statistics.total.prettySpeedAverage[n] = Math.floor(60/state.context.statistics.total.prettySpeedAverage[n]) + ' ms/entry';
         } else {
-          state.context.statistics.total.prettySpeedAverage[n] += ' entries/sec';
+          state.context.statistics.total.prettySpeedAverage[n] += ' ms/entry';
         } 
         if ( state.context.statistics.total.prettySpeedAverage.total < 1 ) {
           state.context.statistics.total.prettySpeedAverage.total = Math.floor(60/state.context.statistics.total.prettySpeedAverage.total) + ' ms/entry';
         } else {
-          state.context.statistics.total.prettySpeedAverage.total += ' entries/sec';
+          state.context.statistics.total.prettySpeedAverage.total += ' ms/entry';
         } 
 
         state.context.statistics.request.prettySpeedRate[v] = Math.floor(state.context.statistics.request.metrics[v].rate);
@@ -518,17 +518,17 @@ window.dashStats = window.dashStats || (function (environment) {
         if ( state.context.statistics.request.prettySpeedRate[v] < 1 ) {
           state.context.statistics.request.prettySpeedRate[v] = Math.floor(60/state.context.statistics.request.prettySpeedRate[v]) + ' ms/entry';
         } else {
-          state.context.statistics.request.prettySpeedRate[v] += ' entries/sec';
+          state.context.statistics.request.prettySpeedRate[v] += ' ms/entry';
         }
         if ( state.context.statistics.request.prettySpeedRate[n] < 1 ) {
           state.context.statistics.request.prettySpeedRate[n] = Math.floor(60/state.context.statistics.request.prettySpeedRate[n]) + ' ms/entry';
         } else {
-          state.context.statistics.request.prettySpeedRate[n] += ' entries/sec';
+          state.context.statistics.request.prettySpeedRate[n] += ' ms/entry';
         } 
         if ( state.context.statistics.request.prettySpeedRate.total < 1 ) {
           state.context.statistics.request.prettySpeedRate.total = Math.floor(60/state.context.statistics.request.prettySpeedRate.total) + ' ms/entry';
         } else {
-          state.context.statistics.request.prettySpeedRate.total += ' entries/sec';
+          state.context.statistics.request.prettySpeedRate.total += ' ms/entry';
         } 
         state.context.statistics.request.prettySpeedAverage[v] = Math.floor(state.context.statistics.request.metrics[v].average);
         state.context.statistics.request.prettySpeedAverage[n] = Math.floor(state.context.statistics.request.metrics[n].average);
@@ -536,17 +536,17 @@ window.dashStats = window.dashStats || (function (environment) {
         if ( state.context.statistics.request.prettySpeedAverage[v] < 1 ) {
           state.context.statistics.request.prettySpeedAverage[v] = Math.floor(60/state.context.statistics.request.prettySpeedAverage[v]) + ' ms/entry';
         } else {
-          state.context.statistics.request.prettySpeedAverage[v] += ' entries/sec';
+          state.context.statistics.request.prettySpeedAverage[v] += ' ms/entry';
         }       
         if ( state.context.statistics.request.prettySpeedAverage[n] < 1 ) {
           state.context.statistics.request.prettySpeedAverage[n] = Math.floor(60/state.context.statistics.request.prettySpeedAverage[n]) + ' ms/entry';
         } else {
-          state.context.statistics.request.prettySpeedAverage[n] += ' entries/sec';
+          state.context.statistics.request.prettySpeedAverage[n] += ' ms/entry';
         } 
         if ( state.context.statistics.request.prettySpeedAverage.total < 1 ) {
           state.context.statistics.request.prettySpeedAverage.total = Math.floor(60/state.context.statistics.request.prettySpeedAverage.total) + ' ms/entry';
         } else {
-          state.context.statistics.request.prettySpeedAverage.total += ' entries/sec';
+          state.context.statistics.request.prettySpeedAverage.total += ' ms/entry';
         } 
 
 
