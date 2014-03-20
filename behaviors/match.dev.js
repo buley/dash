fa8d7a6ddccb4ea68d8c9a0498426163493c2d00
Taylor Buley <taylor.buley@gmail.com>
2014-03-19 23:21:37 -0400

    .

--- behaviors/match.dev.js
+++ behaviors/match.dev.js
@@ -55,7 +55,7 @@ window.dashMatch = window.dashMatch || (function (environment) {
     promise(function(context) {
       //context.type = null;
 	reduced = reduce(state.context.match, context);
-	console.log('matched?',state.context.entry);
+	console.log('matched?',state.context.any);
 	if ( match(reduced, state.context.entry) ) {
 		console.log('matched', reduced, state.context.entry);
 	}
