3aef1205eff5b35bc2dc75f37318e14b5de7e486
Taylor Buley <taylor.buley@gmail.com>
2014-03-19 21:03:53 -0400

    .

--- behaviors/match.dev.js
+++ behaviors/match.dev.js
@@ -5,9 +5,31 @@ window.dashMatch = window.dashMatch || (function (environment) {
       return state;
     }
     var promise = state.promise,
-        deferred = this.deferred();
+        deferred = this.deferred(),
+	that = this,
+	reduce = function(expression, context) {
+		var reducible = false,
+			maybeReduce = function(expr) {
+				if (that.isFunction(expr)) {
+					expr = that.safeApply(expr, [context], that);
+				}
+				if (that.isObject(expr)) {
+					that.iterate(expr, function(key, value) {
+						expr[key] = maybeReduce(value);
+					});
+				}
+				return expr;
+			}
+
+
+		return maybeReduce(expression);
+		
+	},
+	reduced;
     promise(function(context) {
       //context.type = null;
+	reduced = reduce(state.context.match, context);
+	console.log('reduced', reduced);
       deferred.resolve(context);
     });
     state.promise = deferred.promise;
