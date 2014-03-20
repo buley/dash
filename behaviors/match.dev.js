412c0f2099e78d927ba64006f26f7d508365be91
Taylor Buley <taylor.buley@gmail.com>
2014-03-19 21:41:05 -0400

    .

--- behaviors/match.dev.js
+++ behaviors/match.dev.js
@@ -8,28 +8,29 @@ window.dashMatch = window.dashMatch || (function (environment) {
         deferred = this.deferred(),
 	that = this,
 	reduce = function(expression, context) {
-		var reducible = false,
-			maybeReduce = function(expr) {
-				if (that.isFunction(expr)) {
-					expr = that.apply(expr, [context], that);
-				}
-				if (that.isObject(expr)) {
-					that.iterate(expr, function(key, value) {
-						expr[key] = maybeReduce(value);
-					});
-				}
-				return expr;
+		var maybeReduce = function(expr) {
+			if (that.isFunction(expr)) {
+				expr = that.apply(expr, [context], that);
 			}
-
-
-		return maybeReduce(expression);
-		
+			if (that.isObject(expr)) {
+				that.iterate(expr, function(key, value) {
+					expr[key] = maybeReduce(value);
+				});
+			}
+			return expr;
+		}
+		return maybeReduce(expression);	
 	},
+	match = function(expression, data) {
+		var matches = false;
+		console.log('match?', data, expression);
+		return matches;
+	}
 	reduced;
     promise(function(context) {
       //context.type = null;
 	reduced = reduce(state.context.match, context);
-	console.log('reduced', reduced);
+	console.log('reduced', match(reduced, state.context.entry) );
       deferred.resolve(context);
     });
     state.promise = deferred.promise;
