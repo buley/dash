13a104119c7d8f304a493d307f02210734eb98c2
Taylor Buley <taylor.buley@gmail.com>
2014-03-23 11:42:16 -0400

    .

--- behaviors/changes.dev.js
+++ behaviors/changes.dev.js
@@ -2,15 +2,13 @@ window.dashChanges = window.dashAsync || (function (environment) {
   "use strict";
   return [ null, function (state) {
     if(!this.isFunction(state.context.changes)) {
-      console.log('skipping',state.context.changes);
       return state;
     }
-    console.log('getting changes', state.type);
     var promise = state.promise,
         deferred = this.deferred();
     promise(function(ste) {
       setTimeout( function() {
-	      console.log('state',ste);
+	console.log('state',ste);
         deferred.resolve(ste);
       }, 3000 );
     });
