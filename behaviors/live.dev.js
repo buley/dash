016fe117d213c7561a2364e5c2d604f37174be10
Taylor Buley <taylor.buley@gmail.com>
2014-03-23 23:34:31 -0400

    .

--- behaviors/live.dev.js
+++ behaviors/live.dev.js
@@ -9,6 +9,7 @@ window.dashLive = window.dashLive || (function (environment) {
             return;
           }
           console.log('CALL LIVING', ctx.key);
+          defd.resolve(ste);
           defd.notify(ste);
         };
         fn.ready = false;
