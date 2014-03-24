8436204a08cd75c18907bd0de096be04498e8109
Taylor Buley <taylor.buley@gmail.com>
2014-03-24 00:38:17 -0400

    .

--- behaviors/live.dev.js
+++ behaviors/live.dev.js
@@ -8,7 +8,7 @@ window.dashLive = window.dashLive || (function (environment) {
               return;
             }
             st2.method = ste.method;
-            st2.type = 'notify';
+            st2.type = 'resolve';
             changeMap[ ctx.changed ].resolve(st2);
           };
         fn.ready = false;
