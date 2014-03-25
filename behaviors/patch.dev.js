263669e48f4efe2e53c617bdfcfdcd2aa6d9162a
Taylor Buley <taylor.buley@gmail.com>
2014-03-24 23:03:34 -0400

    .

---
+++ behaviors/patch.dev.js
@@ -0,0 +1,49 @@
+window.dashPatch = window.dashPatch || (function (environment) {
+  "use strict";
+  var patchMap = {}; 
+  return [ function (state) {
+    if(this.isEmpty(state.context.patch)) {
+      return state;
+    }
+    state.context.patchid = this.random();
+    patchMap[ state.context.patchid ] = this.isArray(state.context.patch) ? state.context.patch : [state.context.patch];
+    delete state.context.patch;
+    return state;
+  }, function (state) {
+    if(this.isEmpty(state.context.patchid)) {
+      return state;
+    }
+    if (this.exists(state.context.entry)) {
+	    var deferred = this.deferred(),
+	    	result = state.context,
+	    	promise = state.promise,
+	    	results = [],
+	    	promises = [],
+	    	that = this;
+	    this.each(patchMap[ state.context.patchid ], function(fn) {
+	    	result = that.apply(fn, [ result ]);
+		   	if (that.isFunction(result)) {
+		   		promises.push(result);
+		   	} else {
+		   		results.push(result);
+		   	}
+	    });
+	    if (this.isEmpty(promises)) {
+	    	state.context.entry = this.is(results.length, 1) ? results[0] : results;
+	    } else {
+	    	this.each(promises, function(pro) {
+	    		promise = pro(function(result) {
+	    			results.push(results);
+	    		});
+	    	});
+	    	state.context.promise = promise(function(ctx) {
+	    		state.context = that.is(results.length, 1) ? results[0] : results;
+	    		deferred.resolve(state);
+	    	})
+	    }
+	    delete patchMap[ state.context.patchid ];
+	    delete state.context.patchid;
+    }
+    return state;
+  } ];
+}(self));
