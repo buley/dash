bce671796a026bf133caf3f63a163bf0128fda8e
Taylor Buley <taylor.buley@gmail.com>
2014-03-24 22:00:48 -0400

    .

--- behaviors/map.dev.js
+++ behaviors/map.dev.js
@@ -1,18 +1,12 @@
 window.dashMap = window.dashMap || (function (environment) {
   "use strict";
-  var that,
-      mapMap = {}, //heh
-      map = function(ste) {
-      	var ctx = ste.context;
-return fn;
-      };
+  var mapMap = {}; //heh
   return [ function (state) {
-  	that = this;
     if(this.isnt(state.context.map, true)) {
       return state;
     }
     var maps;
-    that = this;
+    this = this;
     state.context.mapd = this.random();
     mapMap[ state.context.mapd ] = this.isArray(state.context.map) ? state.context.map : [state.context.map];
     delete state.context.map;
@@ -28,17 +22,17 @@ return fn;
 	    	results = [],
 	    	promises = [];
 	    this.each(mapMap[ state.context.mapd ], function(fn) {
-	    	result = that.apply(fn, [ state.context ]);
+	    	result = this.apply(fn, [ state.context ]);
 		   	if (this.isFunction(result)) {
 		   		promises.push(result);
 		   	} else {
 		   		results.push(result);
 		   	}
 	    });
-	    if (that.is(promises.length, 0)) {
+	    if (this.is(promises.length, 0)) {
 	    	state.context.entry = is(results.length, 1) ? results[0] : results;
 	    } else {
-	    	that.each(promises, function(pro) {
+	    	this.each(promises, function(pro) {
 	    		promise = promise(pro);
 	    	});
 	    	state.context.promise = promise;
