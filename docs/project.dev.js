1e4e4c183987cb0cad621e42d7aa3d62c1dc859d
Taylor Buley <taylor.buley@gmail.com>
2014-03-27 21:37:30 -0400

    .

--- docs/project.dev.js
+++ docs/project.dev.js
@@ -2,14 +2,12 @@ var dashApp = angular.module('dashApp', ['ngRoute']);
 dashApp.config(['$routeProvider',
   function ($routeProvider) {
     /* Behaviors */
-    /*
     dash.add.behavior(dashStats);
     dash.add.behavior(dashLive);
     dash.add.behavior(dashChanges);
     dash.add.behavior(dashMatch);
     dash.add.behavior(dashCollect);
     dash.add.behavior(dashMap);
-    */
     dash.add.behavior(dashMapReduce);
 
     $routeProvider
