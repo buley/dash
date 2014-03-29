2a0f08c9297152ca875836f1fd2215b2a94ec983
Taylor Buley <taylor.buley@gmail.com>
2014-03-29 13:59:03 -0400

    .

--- behaviors/cache.dev.js
+++ behaviors/cache.dev.js
@@ -510,52 +510,13 @@ window.dashCache = window.dashCache || (function (environment) {
 
 	})();
   return [ function (state) {
-    if(this.isEmpty(state.context.map) || !this.isEmpty(state.context.reduce)) {
+    if(this.isEmpty(state.context.cache)) {
       return state;
     }
-    state.context.mapid = this.random();
-    CREAM[ state.context.mapid ] = this.isArray(state.context.map) ? state.context.map : [state.context.map];
-    delete state.context.map;
     return state;
   }, function (state) {
-    if(this.isEmpty(state.context.mapid) || !this.isEmpty(state.context.reduce)) {
+    if(this.isEmpty(state.context.cache)) {
       return state;
-    }
-    if (this.exists(state.context.entry)) {
-	    var deferred = this.deferred(),
-	    	result = state.context.entry,
-	    	promise = state.promise,
-	    	results = [],
-	    	promises = [],
-	    	that = this;
-	    this.each(CREAM[ state.context.mapid ], function(fn) {
-	    	result = that.apply(fn, [ result ]);
-		   	if (that.isFunction(result)) {
-		   		promises.push(result);
-		   	} else {
-		   		results.push(result);
-		   	}
-	    });
-	    if (this.isEmpty(promises)) {
-	    	state.context.mapped = this.is(results.length, 1) ? results[0] : results;
-	    } else {
-	    	this.each(promises, function(pro) {
-	    		promise(function(result) {
-	    			results.push(result);
-	    		});
-	    		promise = pro;
-	    	});
-	    	state.context.promise = promise(function(ctx) {
-	    		ctx.mapped = that.is(results.length, 1) ? results[0] : results;
-	    		deferred.resolve(ctx);
-	    	}, function(ctx) {
-		        deferred.reject(ctx);
-		    }, function(ctx) {
-		        deferred.notify(ctx);
-		    });
-	    }
-	    delete CREAM[ state.context.mapid ];
-	    delete state.context.mapid;
     }
     return state;
   } ];
