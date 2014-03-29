4d072522aa98c35c33bf207acfdbb68ee1d03478
Taylor Buley <taylor.buley@gmail.com>
2014-03-28 23:47:37 -0400

    .

--- behaviors/patch.dev.js
+++ behaviors/patch.dev.js
@@ -14,7 +14,6 @@ window.dashPatch = window.dashPatch || (function (environment) {
 	patchMap[ state.context.patchid ] = this.isArray(state.context.patch) ? state.context.patch : [state.context.patch, state.context.patch];
     state.promise = deferred.promise;
     result = this.apply(patchMap[ state.context.patchid ][0], [ state ]);
-    console.log('result',result);
     if (this.is(result.promise, state.promise)) {
     	result.promise = promise;
     	state = result;
@@ -33,42 +32,26 @@ window.dashPatch = window.dashPatch || (function (environment) {
     if(this.isEmpty(state.context.patchid)) {
       return state;
     }
-    /*
-    if (this.exists(state.context.entry)) {
-	    var deferred = this.deferred(),
-	    	result = state.context,
-	    	promise = state.promise,
-	    	results = [],
-	    	promises = [],
-	    	that = this;
-	    this.each(patchMap[ state.context.patchid ].after, function(fn) {
-	    	result = that.apply(fn, [ result ]);
-		   	if (that.isFunction(result)) {
-		   		promises.push(result);
-		   	} else {
-		   		results.push(result);
-		   	}
+    var deferred = this.deferred(),
+    	result,
+    	promise = state.promise,
+    	promises = [],
+    	that = this;
+    state.promise = deferred.promise;
+    result = this.apply(patchMap[ state.context.patchid ][1], [ state ]);
+    if (this.is(result.promise, state.promise)) {
+    	result.promise = promise;
+    	state = result;
+    } else {
+		state.context.promise = deferred.promise;
+		result(function(ctx) {
+		  deferred.resolve(ctx);
+		}, function(ctx) {
+	      deferred.reject(ctx);
+	    }, function(ctx) {
+	      deferred.notify(ctx);
 	    });
-	    if (this.isEmpty(promises)) {
-	    	state.context.entry = this.is(results.length, 1) ? results[0] : results;
-	    } else {
-	    	this.each(promises, function(pro) {
-	    		promise = pro(function(result) {
-	    			results.push(results);
-	    		});
-	    	});
-	    	state.context.promise = promise(function(ctx) {
-	    		state.context = that.is(results.length, 1) ? results[0] : results;
-	    		deferred.resolve(state);
-	    	}, function(ctx) {
-		        deferred.reject(ctx);
-		    }, function(ctx) {
-		        deferred.notify(ctx);
-		    })
-	    }
-	    delete patchMap[ state.context.patchid ];
-	    delete state.context.patchid;
-    }*/
+	}
     return state;
   } ];
 }(self));
