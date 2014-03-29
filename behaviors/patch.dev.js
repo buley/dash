d07081bd1a33c84fa34a50aa76a26c067317b180
Taylor Buley <taylor.buley@gmail.com>
2014-03-29 00:36:09 -0400

    .

--- behaviors/patch.dev.js
+++ behaviors/patch.dev.js
@@ -5,26 +5,29 @@ window.dashPatch = window.dashPatch || (function (environment) {
     if(this.isEmpty(state.context.patch)) {
       return state;
     }
-    var deferred = this.deferred(),
+    var outside = this.deferred(),
+	    inside = this.deferred(),
     	result,
     	promise = state.promise,
     	promises = [],
     	that = this;
     state.context.patchid = this.random();
 	patchMap[ state.context.patchid ] = this.isArray(state.context.patch) ? state.context.patch : [state.context.patch, state.context.patch];
-    state.promise = deferred.promise;
+    state.promise = outside.promise;
     result = this.apply(patchMap[ state.context.patchid ][0], [ state ]);
     if (this.is(result.promise, state.promise)) {
     	result.promise = promise;
     	state = result;
     } else {
-    	state.context.promise = deferred.promise;
 		result(function(ctx) {
-    		deferred.resolve(ctx);
+	    	state.context.promise = promise;
+    		outside.resolve(ctx);
     	}, function(ctx) {
-	        deferred.reject(ctx);
+	    	state.context.promise = promise;
+	        outside.reject(ctx);
 	    }, function(ctx) {
-	        deferred.notify(ctx);
+	    	state.context.promise = promise;
+	        outside.notify(ctx);
 	    });
 	}
     return state;
@@ -33,7 +36,7 @@ window.dashPatch = window.dashPatch || (function (environment) {
       return state;
     }
     if (!patchMap[ state.context.patchid ][1]) {
-    	console.log('wtf',state, patchMap[ state.context.patchid ]);
+    	console.log('wtf',patchMap[ state.context.patchid ]);
     	return state;
     }
     var outside = this.deferred(),
