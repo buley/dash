cc16470a9cf823421764acf29b889366faa17a01
Taylor Buley <taylor.buley@gmail.com>
2014-03-29 18:27:05 -0400

    .

--- behaviors/cache.dev.js
+++ behaviors/cache.dev.js
@@ -115,25 +115,24 @@ window.dashCache = window.dashCache || (function (environment) {
     	outward = this.deferred(),
     	response;
     if (this.contains(['get.entry'], state.method)) {
+    	console.log('was',state);
 	    response = get( {key: buildKey(state.context) });
+	    if (!this.isEmpty(response)) {
+	    	state = response;
+	    	state.context.cached = true;
+	    	setTimeout(function(){
+		    	outward.resolve(state);
+	    	}, 1000);
+	    	state.promise = outward;
+	    }
 	    console.log("CREAM get", buildKey(state.context), state);
-		promise(function(ctx) {
-		    if (!this.isEmpty(response)) {
-		    	state = response;
-		    	state.context.cached = true;
-		    	state.promise = outward;
-		    }
+		/*promise(function(ctx) {
 	      outward.resolve(ctx);
 	    }, function(ctx) {
 	      outward.error(ctx);
 	    }, function(ctx) {
-	    	if (!this.isEmpty(response)) {
-		    	ctx = response;
-		    	ctx.context.cached = true;
-		    	ctx.promise = outward;
-		    }
 	      outward.notify(ctx);
-	    });
+	    });*/
     }
     return state;
   }, function (state) {
