7343c87cdaad3e708ab60c7d32ad6b4140993e95
Taylor Buley <taylor.buley@gmail.com>
2014-03-29 20:45:09 -0400

    .

--- behaviors/cache.dev.js
+++ behaviors/cache.dev.js
@@ -24,6 +24,7 @@ window.dashCache = window.dashCache || (function (environment) {
 			return;
 		}
 		if(cache[ key ]) {
+			console.log(cache[ key ].expire,'?');
 			if(cache[ key ].expire > new Date().getTime()) {
 				return cache[ key ].data;
 			}
