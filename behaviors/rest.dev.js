89d8c3be471ffc179d9395c10462d7403ecc5d60
Taylor Buley <taylor.buley@gmail.com>
2014-03-30 21:41:53 -0400

    .

--- behaviors/rest.dev.js
+++ behaviors/rest.dev.js
@@ -198,10 +198,8 @@ self.dashRest = self.dashRest || (function (environment) {
       that.iterate(callbacks, function (key, val) {
         delete context[key];
       });
-      workRegister(worker, message, context, function (data) {
-      	console.log('REST resolved, store?',data.entry, typeof that.api.add.entry);
-      	var add_ctx = that.clone(context);
-      	add_ctx.data = data.entry;
+      workRegister(worker, message, context, function (add_ctx) {
+      	console.log('REST resolved, store?',data.context.entry, typeof that.api.add.entry);
       	that.api.add.entry(add_ctx)(function(added_ctx) {
 	        defd.resolve(getData(added_ctx));
       	}, function(added_ctx) {
