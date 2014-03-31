3af4c63bc458e819904fa6fc397beaab3751cbe9
Taylor Buley <taylor.buley@gmail.com>
2014-03-30 20:20:03 -0400

    .

--- behaviors/rest.dev.js
+++ behaviors/rest.dev.js
@@ -257,7 +257,7 @@ self.dashRest = self.dashRest || (function (environment) {
   } else {
 	  return [ function (state) {
 	  	that = this;
-	    if(this.isEmpty(state.context.rest) && this.isnt(state.context.rest, false)) {
+	    if(this.isEmpty(state.context.rest) || this.isnt(state.context.rest, false)) {
 	      return state;
 	    }
 	    state.context.restid = this.random();
