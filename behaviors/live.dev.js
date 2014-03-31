db4687f0f39e27b496847d8c1a32ca140709623a
Taylor Buley <taylor.buley@gmail.com>
2014-03-30 23:13:30 -0400

    .

--- behaviors/live.dev.js
+++ behaviors/live.dev.js
@@ -9,6 +9,7 @@ window.dashLive = window.dashLive || (function (environment) {
               return live_ctx;
             }
             ste.type = 'notify';
+            ste.context = live_ctx.context;
             ste.context.relived = true;
             liveMap[ ste.context.liveid ].resolve(ste);
           };
