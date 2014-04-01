f27b3ed51eaf603ce0f925d55dfa0575667079d2
Taylor Buley <taylor.buley@gmail.com>
2014-03-31 21:15:24 -0400

    .

--- behaviors/live.dev.js
+++ behaviors/live.dev.js
@@ -6,7 +6,6 @@ window.dashLive = window.dashLive || (function (environment) {
         var ctx = ste.context,
           fn = function(live_ctx) {
             if (!liveMap[ ste.context.liveid ] ||( !!live_ctx.context && !!live_ctx.context.zombie)) {
-              delete live_ctx.context.zombie;
               return live_ctx;
             }
             ste.type = 'notify';
