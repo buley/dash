7e68030f85ef1ae67532c475232426e8070646ec
Taylor Buley <taylor.buley@gmail.com>
2014-04-02 22:18:27 -0400

    !fixup

--- behaviors/firebase.dev.js
+++ behaviors/firebase.dev.js
@@ -1,70 +1,70 @@
 self.dashFirebase = self.dashFirebase || (function (environment) {
   "use strict";
   var that,
-	child = function( context ) {	
-		var deferred = that.deferred();
-		context.method = 'child';
-		return deferred.promise;
-	},
-	set = function( context ) {	
-		var deferred = that.deferred(),
-			ref = firebase[ [context.firebase, context.database, context.store ].join('/') ].child(context.primary_key);
-		context.method = 'set';
-		ref.set( context.entry, function(err) {
-			if(that.err, null) {
-				deferred.resolve(context.entry);
-			} else {
-				deferred.reject(err);
-			}
-		} );
-		return deferred.promise;
-	},
-	update = function( context ) {	
-		var deferred = that.deferred(),
-			ref = firebase[ [context.firebase, context.database, context.store ].join('/') ].child(context.primary_key);
-		context.method = 'update';
-		ref.update(context.entry, function() {
-			if(that.err, null) {
-				deferred.resolve(context.entry);
-			} else {
-				deferred.reject(err);
-			}
-		});
-		return deferred.promise;
-	},
-	remove = function( context ) {	
-		var deferred = that.deferred(),
-			ref = firebase[ [context.firebase, context.database, context.store ].join('/') ].child(context.primary_key);
-		context.method = 'remove';
-		ref.remove(function() {
-			if(that.err, null) {
-				deferred.resolve(context.entry);
-			} else {
-				deferred.reject(err);
-			}
-		});
-		return deferred.promise;
-	},
-	whichMethod = function(signature) {
-		if ( that.contains( [ 'get.entry', 'get.entries', 'get.index', 'get.database', 'get.store' ], signature)) {
-			return 'child';
-		} else if ( that.contains([ 'remove.entry', 'remove.entries', 'remove.index', 'remove.database', 'remove.store' ], signature)) {
-			return 'remove';
-		} else if ( that.contains([ 'add.entry' ], signature)) {
-			return 'set';
-		} else if ( that.contains([ 'update.entry', 'update.entries' ], signature)) {
-			return 'update';
-		} else {
-			return null;
-		}
-	},
-	scripts = ( !! environment.document) ? environment.document.getElementsByTagName("script") : [],
+    child = function (context) {
+      var deferred = that.deferred();
+      context.method = 'child';
+      return deferred.promise;
+    },
+    set = function (context) {
+      var deferred = that.deferred(),
+        ref = firebase[[context.firebase, context.database, context.store].join('/')].child(context.primary_key);
+      context.method = 'set';
+      ref.set(context.entry, function (err) {
+        if (that.err, null) {
+          deferred.resolve(context.entry);
+        } else {
+          deferred.reject(err);
+        }
+      });
+      return deferred.promise;
+    },
+    update = function (context) {
+      var deferred = that.deferred(),
+        ref = firebase[[context.firebase, context.database, context.store].join('/')].child(context.primary_key);
+      context.method = 'update';
+      ref.update(context.entry, function () {
+        if (that.err, null) {
+          deferred.resolve(context.entry);
+        } else {
+          deferred.reject(err);
+        }
+      });
+      return deferred.promise;
+    },
+    remove = function (context) {
+      var deferred = that.deferred(),
+        ref = firebase[[context.firebase, context.database, context.store].join('/')].child(context.primary_key);
+      context.method = 'remove';
+      ref.remove(function () {
+        if (that.err, null) {
+          deferred.resolve(context.entry);
+        } else {
+          deferred.reject(err);
+        }
+      });
+      return deferred.promise;
+    },
+    whichMethod = function (signature) {
+      if (that.contains(['get.entry', 'get.entries', 'get.index', 'get.database', 'get.store'], signature)) {
+        return 'child';
+      } else if (that.contains(['remove.entry', 'remove.entries', 'remove.index', 'remove.database', 'remove.store'], signature)) {
+        return 'remove';
+      } else if (that.contains(['add.entry'], signature)) {
+        return 'set';
+      } else if (that.contains(['update.entry', 'update.entries'], signature)) {
+        return 'update';
+      } else {
+        return null;
+      }
+    },
+    scripts = ( !! environment.document) ? environment.document.getElementsByTagName("script") : [],
     libraryScript = scripts[scripts.length - 1] || null,
-    libraryPath =( null !== libraryScript && null === libraryScript.src.match(/chrome-extension/) ) ? libraryScript.src : null,
-	workerEnvironment = null !== environment.constructor.toString().match(/WorkerGlobalScope/),
-	worker = workerEnvironment ? null : new Worker(libraryPath),
-	workQueue = {},
-	firebases = {},
+    libraryPath = (null !== libraryScript && null === libraryScript.src.match(/chrome-extension/)) ? libraryScript.src : null,
+    workerEnvironment = null !== environment.constructor.toString().match(/WorkerGlobalScope/),
+    worker = workerEnvironment ? null : new Worker(libraryPath),
+    workQueue = {},
+    firebases = {},
     workRegister = function (worker, message, context, success, error, notify) {
       var id = that.random(),
         callback = function (e) {
@@ -90,19 +90,19 @@ self.dashFirebase = self.dashFirebase || (function (environment) {
             }
           }
         },
-        clean = function(obj) {
-          if (that.isFunction(obj)) { 
+        clean = function (obj) {
+          if (that.isFunction(obj)) {
             return undefined;
           } else if (that.isObject(obj)) {
-            that.iterate(obj, function(key, val) {
-              obj[ key ] = clean(val);
+            that.iterate(obj, function (key, val) {
+              obj[key] = clean(val);
             });
           } else if (that.isArray(obj)) {
-            that.each(obj, function(v, i) {
+            that.each(obj, function (v, i) {
               obj[i] = clean(v);
             });
           }
-          return obj;          
+          return obj;
         };
       workQueue[id] = {
         success: success,
@@ -129,32 +129,32 @@ self.dashFirebase = self.dashFirebase || (function (environment) {
           on_close: context.on_close
         },
         getData = function (data) {
-          if( that.isObject(data) )  {
-	          that.iterate(callbacks, function (key, val) {
-	          	if(!!val) {
-		            data[key] = val;
-	          	}
-	          });
-  		  }
+          if (that.isObject(data)) {
+            that.iterate(callbacks, function (key, val) {
+              if ( !! val) {
+                data[key] = val;
+              }
+            });
+          }
           return data;
         };
       that.iterate(callbacks, function (key, val) {
         delete context[key];
       });
       workRegister(worker, message, context, function (data) {
-      	var add_ctx = that.clone(context);
-      	add_ctx.data = data.context.entry;
-      	add_ctx.firebaseing = true;
-      	that.api.add.entry(add_ctx)(function(added_ctx) {
-      		delete added_ctx.firebaseing;
-	        defd.resolve(getData(added_ctx));
-      	}, function(added_ctx) {
-      		delete added_ctx.firebaseing;
-	        defd.reject(getData(added_ctx));
-      	}, function(added_ctx) {
-      		delete added_ctx.firebaseing;
-	        defd.notify(getData(added_ctx));
-      	})
+        var add_ctx = that.clone(context);
+        add_ctx.data = data.context.entry;
+        add_ctx.firebaseing = true;
+        that.api.add.entry(add_ctx)(function (added_ctx) {
+          delete added_ctx.firebaseing;
+          defd.resolve(getData(added_ctx));
+        }, function (added_ctx) {
+          delete added_ctx.firebaseing;
+          defd.reject(getData(added_ctx));
+        }, function (added_ctx) {
+          delete added_ctx.firebaseing;
+          defd.notify(getData(added_ctx));
+        })
       }, function (data) {
         defd.reject(getData(data));
       }, function (data) {
@@ -162,10 +162,9 @@ self.dashFirebase = self.dashFirebase || (function (environment) {
       });
       return defd.promise;
     };
-
   if (true === workerEnvironment) {
-  	importScripts('https://cdn.firebase.com/js/client/1.0.6/firebase.js');
-  	console.log('firebase',Firebase);
+    importScripts('https://cdn.firebase.com/js/client/1.0.6/firebase.js');
+    console.log('firebase', Firebase);
     environment.addEventListener('message', function (e) {
       var input = e.data,
         method = input.method,
@@ -177,33 +176,33 @@ self.dashFirebase = self.dashFirebase || (function (environment) {
         end = function (ctx) {
           ctx.type = ctx.type || 'success';
           environment.postMessage(ctx);
-        }, 
-        callback = function(sig) {
-	      	return function(data, error) {
-	      		delete input.context.callback;
-	      		if (!!error) {
-	      			input.context.error = error;
-	      			input.context.message = data;
-	      			input.type = 'error';
-	      		} else {
-	      			input.context.entry = data;
-	      		}
-			    end(input);
-	      	}
-      	};
-  	  if ( 'undefined' === typeof firebases[input.context.firebase] ) {
-  		  firebases[input.context.firebase] =  new Firebase([context.firebase, context.database, context.store ].join('/'));
-  	  }
-      if (that.contains([ 'set', 'update', 'remove', 'child' ], method)) {
-      	context.callback = callback(method);
-        if ( method === 'set' ) {
-        	set(context);
-        } else if ( method === 'child' ) {
-        	child(context);
-        } else if ( method === 'update' ) {
-        	update(context);
-        } else if ( method === 'remove' ) {
-        	remove(context);
+        },
+        callback = function (sig) {
+          return function (data, error) {
+            delete input.context.callback;
+            if ( !! error) {
+              input.context.error = error;
+              input.context.message = data;
+              input.type = 'error';
+            } else {
+              input.context.entry = data;
+            }
+            end(input);
+          }
+        };
+      if ('undefined' === typeof firebases[input.context.firebase]) {
+        firebases[input.context.firebase] = new Firebase([context.firebase, context.database, context.store].join('/'));
+      }
+      if (that.contains(['set', 'update', 'remove', 'child'], method)) {
+        context.callback = callback(method);
+        if (method === 'set') {
+          set(context);
+        } else if (method === 'child') {
+          child(context);
+        } else if (method === 'update') {
+          update(context);
+        } else if (method === 'remove') {
+          remove(context);
         }
       } else {
         input.type = 'error';
@@ -215,55 +214,80 @@ self.dashFirebase = self.dashFirebase || (function (environment) {
       }
     }, false);
   } else {
-	  return [ function (state) {
-	  	that = this;
-	    if(this.isEmpty(state.context.firebase) || this.exists(state.context.firebaseing)) {
-	      return state;
-	    }
-	    state.context.firebaseid = this.random();
-	    firebase[ state.context.firebaseid ] = {
-	    	url: state.context.url,
-	    	params: state.context.params ? state.context.params : null
-	    }
-	    delete state.context.url;
-	    delete state.context.params;
-	    return state;
-	  }, function (state) {
-      if(this.isEmpty(state.context.firebase) || this.exists(state.context.firebaseing)) {
-	      return state;
-	    }
-	    var promise = state.promise,
-	    	outward = this.deferred(),
-	    	inward,
-	    	update = false,
-	    	args;
-	    	if (this.contains(['add.entry', 'update.entry', 'update.entries', 'remove.entry', 'remove.entries'], state.method)) {
-			    if (this.contains(['notify', 'resolve'], state.type)) {
-			      update = true;
-			    } 
-	    	} else {
-	    		if ((this.is('error', state.type) || (this.isEmpty(state.context.entry) && this.contains(['get.entry', 'get.entries'], state.method))) && (this.is(state.context.firebase, true) || this.is(state.context.fallback, true))) {
-			      update = true;
-				}
-	    	}
-	    	if (update) {
-    		  state.promise = outward.promise;
-	        inward = workDispatch( whichMethod(state.method), state.context);
-		  	  inward(function(ctx2){
-    		    state.context = ctx2;
-    		    state.type = 'resolve';
-			     outward.resolve(state.context);
-		  	  }, function(ctx2) {
-    		    state.context = ctx2;
-    		    state.type = 'error';
-  			    outward.reject(state.context);
-		  	  }, function(ctx2) {
-    		    state.type = 'notify';
-    		    state.context = ctx2;
-			       outward.notify(state.context);
-		  	  });
-	    	}
-	    return state;
-	  } ];
-	}
+    return [function (state) {
+      that = this;
+      if (this.isEmpty(state.context.firebase) || this.exists(state.context.firebaseing)) {
+        return state;
+      }
+      if(this.is(state.context.sync, true)) {
+        var promise = state.promise,
+        outward = this.deferred(),
+        inward,
+        update = false,
+        args;
+      if (this.contains(['add.entry', 'update.entry', 'update.entries', 'remove.entry', 'remove.entries'], state.method)) {
+        if (this.contains(['notify', 'resolve'], state.type)) {
+          update = true;
+        }
+      } else {
+        if ((this.is('error', state.type) || (this.isEmpty(state.context.entry) && this.contains(['get.entry', 'get.entries'], state.method))) && (this.is(state.context.firebase, true) || this.is(state.context.fallback, true))) {
+          update = true;
+        }
+      }
+      if (update) {
+        state.promise = outward.promise;
+        inward = workDispatch(whichMethod(state.method), state.context);
+        inward(function (ctx2) {
+          state.context = ctx2;
+          state.type = 'resolve';
+          outward.resolve(state.context);
+        }, function (ctx2) {
+          state.context = ctx2;
+          state.type = 'error';
+          outward.reject(state.context);
+        }, function (ctx2) {
+          state.type = 'notify';
+          state.context = ctx2;
+          outward.notify(state.context);
+        });
+      }
+      return state;
+    }, function (state) {
+      if (this.isEmpty(state.context.firebase) || this.exists(state.context.firebaseing)) {
+        return state;
+      }
+      var promise = state.promise,
+        outward = this.deferred(),
+        inward,
+        update = false,
+        args;
+      if (this.contains(['add.entry', 'update.entry', 'update.entries', 'remove.entry', 'remove.entries'], state.method)) {
+        if (this.contains(['notify', 'resolve'], state.type)) {
+          update = true;
+        }
+      } else {
+        if ((this.is('error', state.type) || (this.isEmpty(state.context.entry) && this.contains(['get.entry', 'get.entries'], state.method))) && (this.is(state.context.firebase, true) || this.is(state.context.fallback, true))) {
+          update = true;
+        }
+      }
+      if (update) {
+        state.promise = outward.promise;
+        inward = workDispatch(whichMethod(state.method), state.context);
+        inward(function (ctx2) {
+          state.context = ctx2;
+          state.type = 'resolve';
+          outward.resolve(state.context);
+        }, function (ctx2) {
+          state.context = ctx2;
+          state.type = 'error';
+          outward.reject(state.context);
+        }, function (ctx2) {
+          state.type = 'notify';
+          state.context = ctx2;
+          outward.notify(state.context);
+        });
+      }
+      return state;
+    }];
+  }
 }(self));
