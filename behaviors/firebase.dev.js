9b8f296598323c0371413040b172841192525090
Taylor Buley <taylor.buley@gmail.com>
2014-04-05 20:28:32 -0400

    .

--- behaviors/firebase.dev.js
+++ behaviors/firebase.dev.js
@@ -459,7 +459,7 @@ self.dashFirebase = self.dashFirebase || (function (environment) {
                     remotepro(function(ctx2) {
                       var extra = that.clone(ctx2.context),
                           update_pro;
-                      extra.entry = that.clone(local);
+                      extra.entry = that.clone(remote);
                       extra.firerebasing = true;
                       update_pro = workDispatch(whichMethod(ctx2.method), extra, 'update', ctx2.type);
                       update_pro(function(ctx3) {
