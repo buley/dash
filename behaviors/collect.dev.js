e042745e0354f4f2ccf4fef7462c4a9f764d6037
Taylor Buley <taylor.buley@gmail.com>
2014-09-14 23:15:39 -0700

    .

--- behaviors/collect.dev.js
+++ behaviors/collect.dev.js
@@ -1,41 +1,40 @@
-self.dashCollect = self.dashCollect || (function (environment) {
+self.dashCollect = self.dashCollect || (function(environment) {
   "use strict";
   var collections = {};
-  return [ function (state) {
-    if(this.isnt(state.context.collect, true)) {
+  return [function(state) {
+    if (this.isnt(state.context.collect, true)) {
       return state;
     }
     state.context.collector = this.random();
-    collections[ state.context.collector ] = [];
-    state.context.collection = collections[ state.context.collector ];
+    collections[state.context.collector] = [];
+    state.context.collection = collections[state.context.collector];
     return state;
-  }, function (state) {
-    if(this.isnt(state.context.collect, true)) {
+  }, function(state) {
+    if (this.isnt(state.context.collect, true)) {
       return state;
     }
     var promise = state.promise,
-        deferred = this.deferred(),
-        that = this;
+      deferred = this.deferred(),
+      that = this;
     promise(function(ste) {
-      if (that.exists(ste.context.entry)) { 
-        collections[ ste.context.collector ].push(ste.context.entry);
-        ste.context.collection = that.clone(collections[ ste.context.collector ]);
+      if (that.exists(ste.context.collector)) {
+        ste.context.collection = that.clone(collections[ste.context.collector]);
       }
-	  delete ste.collector;
+      delete ste.collector;
       deferred.resolve(ste);
     }, function(ctx) {
-	  delete ctx.collector;
+      delete ctx.collector;
       deferred.reject(ctx);
-      delete collections[ ctx.context.collector ];
+      delete collections[ctx.context.collector];
     }, function(ctx) {
-		if (that.exists(ste.context.entry)) { 
-		  collections[ ste.context.collector ].push(ste.context.entry);
-		  ste.context.collection = that.clone(collections[ ste.context.collector ]);
-		}
-		delete ctx.collector;
-        deferred.notify(ctx);
+      if (that.exists(ste.context.entry)) {
+        collections[ste.context.collector].push(ste.context.entry);
+        ste.context.collection = that.clone(collections[ste.context.collector]);
+      }
+      delete ctx.collector;
+      deferred.notify(ctx);
     });
     state.promise = deferred.promise;
     return state;
-  } ];
+  }];
 }(self));
