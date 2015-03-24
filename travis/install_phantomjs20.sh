ca3a17e9c65a9a0923ed00ab6af8f3f885e981f5
Taylor Buley <buley@outlook.com>
2015-03-24 15:38:19 -0700

    trying to get travis building again

--- travis/install_phantomjs20.sh
+++ travis/install_phantomjs20.sh
@@ -2,6 +2,7 @@ cd travis
 #wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-i686.tar.bz2
 #wget https://s3.amazonaws.com/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-i686.tar.bz2
-tar -xjf phantomjs-2.0.0-ubuntu-12.04.tar.bz2
+#tar -xjf phantomjs-2.0.0-ubuntu-12.04.tar.bz2
+tar -xjf phantomjs-1.9.7-linux-i686.tar.bz2
 sudo rm -rf /usr/local/phantomjs/bin/phantomjs
 sudo mv phantomjs /usr/local/phantomjs/bin/phantomjs
