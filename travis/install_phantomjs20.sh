5849104ae7f792a74f30a0ca6b02d83b82bfbb27
Taylor Buley <buley@outlook.com>
2015-03-24 13:23:05 -0700

    trying to get travis building again

--- travis/install_phantomjs20.sh
+++ travis/install_phantomjs20.sh
@@ -1,10 +1,14 @@
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-source.zip
-unzip phantomjs-2.0.0-source.zip
-sudo apt-get install build-essential g++ flex bison gperf ruby perl \
-  libsqlite3-dev libfontconfig1-dev libicu-dev libfreetype6 libssl-dev \
-  libpng-dev libjpeg-dev
+#wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-source.zip
+#unzip phantomjs-2.0.0-source.zip
+#sudo apt-get install build-essential g++ flex bison gperf ruby perl \
+#  libsqlite3-dev libfontconfig1-dev libicu-dev libfreetype6 libssl-dev \
+#  libpng-dev libjpeg-dev
 cd travis
-git clone git://github.com/ariya/phantomjs.git
-cd phantomjs
-git checkout 2.0
-./build.sh
+wget https://s3.amazonaws.com/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2
+tar -xvvf phantomjs-2.0.0-ubuntu-12.04.tar.bz2
+cd phantomjs-2.0.0-ubuntu-12.04
+ls -la .
+#git clone git://github.com/ariya/phantomjs.git
+#cd phantomjs
+#git checkout 2.0
+#./build.sh
