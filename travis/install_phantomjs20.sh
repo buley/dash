4221750dec9bfff4c515f23622d455ce492a9c8a
Taylor Buley <buley@outlook.com>
2015-03-24 21:22:55 -0700

    travis

--- travis/install_phantomjs20.sh
+++ travis/install_phantomjs20.sh
@@ -1,9 +1,11 @@
 cd travis
 #wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-i686.tar.bz2
 #wget https://s3.amazonaws.com/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-i686.tar.bz2
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
 #tar -xjf phantomjs-2.0.0-ubuntu-12.04.tar.bz2
-tar -xjf phantomjs-1.9.7-linux-i686.tar.bz2
-ls -la phantomjs-1.9.7-linux-i686/bin
+tar -xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
+ls -la .
+ls -la phantomjs-1.9.7-linux-x86_64/bin
 sudo rm -rf /usr/local/phantomjs/bin/phantomjs
-sudo mv phantomjs-1.9.7-linux-i686/bin/phantomjs /usr/local/phantomjs/bin/phantomjs
+sudo mv phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/phantomjs/bin/phantomjs
+echo "OK"
